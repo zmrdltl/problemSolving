83235b7f555795b11699d55f76fb64432d48d89b
zmrdltl <meenseek5929@naver.com>
2020-09-08 14:44:23 +0900

    백준 재귀z

--- boj/Implemenation/1074.cpp
+++ boj/Implemenation/1074.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int n,r,c,num=0;
-int board[3000][3000];
+int n,r,c,ans=0;
+int dx[4] = {0,0,1,1};
+int dy[4] = {0,1,0,1};
 
-//TODO 못함
-
-//z모양으로 숫자 채우기
-void putNumInBoard(int row,int col,int &num,int board[3000][3000]){
-    for(int i = row; i< row + 2; i++){
-        for(int j = col; j < col + 2; j++){
-            board[i][j] = num++;
+void fillBoard(int n,int x,int y){
+    if(n==2){
+        for(int i = 0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx== r && ny == c){
+                cout << ans;
+                return;
+            }
+            ans++;
         }
+        return;
     }
+    fillBoard(n/2,x,y);
+    fillBoard(n/2,x,y+n/2);
+    fillBoard(n/2,x+n/2,y);
+    fillBoard(n/2,x+n/2,y+n/2);
 }
 
 int main(){
-    
     cin >> n >> r >> c; // 2*n X 2*n의 판에서 (r,c)는 무슨 숫자?
-    int MAX = pow(2,n);
-    for(int i = 0; i < MAX; i+=2){
-        for(int j = 0; j < MAX; j+=2){
-            putNumInBoard(i,j,num,board);
-        }
-    }
-    /*for(int i = 0; i < MAX; i++){
-        for(int j = 0; j < MAX; j++){
-            cout << board[i][j] <<  ' ';
-        }
-        cout << '\n';
-    }*/
-    cout << board[r][c];
+    fillBoard(1<<n,0,0);
 }
