b4cfa33d0121c3231c11d99d1af0cf7bd8fb6807
zmrdltl <meenseek5929@naver.com>
2020-09-14 01:12:12 +0900

    이분탐색

---
+++ boj/binarySearch/1937.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+vector <int> liquid;
+int n;
+void binarySearch(int n){
+    int left = 0;
+    int right = n-1;
+    int rightValue, leftValue, sum = 0;
+    int result = 0x7f7f7f7f;
+    while(left<right){
+        sum = (liquid[left] + liquid[right]);
+        if(abs(result) > abs(sum)){
+            result = sum;
+            leftValue = liquid[left];
+            rightValue = liquid[right];
+        }
+        if(sum<=0) left++;
+        else right--;
+    }
+    if(leftValue < rightValue)
+        cout << leftValue <<  ' ' <<  rightValue <<'\n'; 
+    else
+        cout << rightValue  <<  ' ' <<  leftValue <<'\n'; 
+}
+int main(){
+    cin >> n;
+    for(int i = 0; i <n; i++){
+        int feature;
+        cin >> feature;
+        liquid.push_back(feature);
+    }
+    sort(liquid.begin(),liquid.end());
+    binarySearch(n);
+}
