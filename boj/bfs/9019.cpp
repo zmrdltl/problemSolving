c2ed4f9437e26d1948643202a595c52888997620
zmrdltl <meenseek5929@naver.com>
2020-09-18 20:13:33 +0900

    dfs

---
+++ boj/bfs/9019.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int testCase, a, b;
+
+string dfs(string ans, int num){
+
+}
+int main(){
+    cin >> testCase;
+    while(testCase--){
+        cin >> a >> b;
+    }
+}
