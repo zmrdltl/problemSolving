634d3c8b4296b11c91b026fc19682985f5c4ff06
zmrdltl <meenseek5929@naver.com>
2020-09-21 00:58:10 +0900

    two pointer

---
+++ boj/binary_search/2473.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+using namespace std;
+ll liquid[5001];
+int n;
+ll answerArr[3];
+void twoPointer(){
+    ll result =  3*10e9+10;
+    for(int first = 0; first < n - 2; first++){
+        ll left = first+1;
+        ll right = n-1;
+        while(left < right){
+            ll sum = liquid[left] + liquid[right] + liquid[first];
+
+            if(abs(result) > abs(sum)){
+                result = sum;
+                answerArr[0] = liquid[first];
+                answerArr[1] = liquid[left];
+                answerArr[2] = liquid[right];
+            }
+            if(sum>=0) right--;
+            else left++;
+        }   
+    }
+    sort(answerArr,answerArr+3);
+}
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> liquid[i];
+    sort(liquid,liquid+n);
+    twoPointer();
+    for(int i = 0; i < 3; i++) cout << answerArr[i] << ' ';
+}
