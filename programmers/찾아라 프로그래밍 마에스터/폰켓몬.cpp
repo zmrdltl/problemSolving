7fac528a6e331ef011805e07320430ff06554293
zmrdltl <meenseek5929@naver.com>
2021-01-01 20:40:22 +0900

    구현

---
+++ programmers/찾아라 프로그래밍 마에스터/폰켓몬.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int cnt[200001];
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    int size = nums.size();
+    int category = 0;
+    for(int i = 0; i < size; i++){
+        if(!cnt[nums[i]]) category++;
+        cnt[nums[i]]++;
+    }
+
+    return min(category,size/2);
+}
