fd039465f8f0d58d9a9f383ec900af46d2cbef83
zmrdltl <meenseek5929@naver.com>
2021-01-05 12:51:13 +0900

    추석트래픽

--- programmers/2018_kakao_blind_recruitment/[1차]추석 트래픽.js
+++ programmers/2018_kakao_blind_recruitment/[1차]추석 트래픽.js
@@ -1,4 +1,34 @@
 function solution(lines) {
   var answer = 0;
+  const arr = [];
+  const time = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].split(" ");
+    const endSecond =
+      Number(line[1].substring(0, 2)) * 3600 +
+      Number(line[1].substring(3, 5)) * 60 +
+      Number(line[1].substring(6, 12));
+    const duration = Number(line[2].substring(0, line[2].length - 1));
+    const startSecond = endSecond - duration + 0.001;
+    arr.push([startSecond, endSecond]);
+    time.push(startSecond, endSecond);
+  }
+  time.sort();
+  for (let i = 0; i < time.length; i++) {
+    const start = time[i];
+    const end = time[i] + 1.0;
+    let count = 0;
+    for (let j = 0; j < arr.length; j++) {
+      const startSecond = arr[j][0];
+      const endSecond = arr[j][1];
+      if (
+        (start <= startSecond && startSecond < end) ||
+        (start <= endSecond && endSecond < end) ||
+        (start >= startSecond && endSecond >= end)
+      )
+        count += 1;
+    }
+    answer = Math.max(answer, count);
+  }
   return answer;
 }
--- programmers/Summer/Winter_Coding/예산.py
+++ programmers/Summer/Winter_Coding/예산.py
@@ -1,19 +1,5 @@
-#include <iostream>
-#include <stdio.h>
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
-int ck[101];
-int answer= 0 ;
-
-int solution(vector<int> d, int budget) {
-    sort(d.begin(),d.end());
-    int cur = 0;
-    for(int i = 0; i < d.size(); i++){
-        ck[cur+d[i]]++;
-        cur = d[i];
-    }
-
-    return d[budget];
-}
+def solution(d, budget):
+    d.sort() 
+    while budget < sum(d):
+        d.pop() 
+    return len(d)
