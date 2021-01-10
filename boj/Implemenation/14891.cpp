20791d9eef53f48e0a69887583ee2b027946bd4f
zmrdltl <meenseek5929@naver.com>
2021-01-10 21:00:24 +0900

    2020.01.10

--- boj/Implemenation/14891.cpp
+++ boj/Implemenation/14891.cpp
@@ -1,91 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int direct[4];
-vector<vector<int>> cgear;
-void fillDirect(int start, int direction) {
+int direction[4];
+vector<vector<int>> gear;
+
+vector <int> getGearVector(string gearInfo){
+    vector <int> tmpGear;
+    for(int i = 0; i < 8; i++)
+        tmpGear.push_back(gearInfo[i] - '0');
+    return tmpGear;
+}
+
+void fillDirection(int start, int rotateDirection) {
 	int next = start + 1;
 	int prev = start - 1;
-	direct[start] = direction;
-	if (next < 4 && direct[next] == 0 && cgear[start][2] != cgear[next][6]) {
-		fillDirect(next, direction * -1);
+	direction[start] = rotateDirection;
+	if (next < 4 && direction[next] == 0 && gear[start][2] != gear[next][6]) {
+		fillDirection(next, rotateDirection * -1);
 	}
-	if (prev >= 0 && direct[prev] == 0 && cgear[start][6] != cgear[prev][2]) {
-		fillDirect(prev, direction * -1);
+	if (prev >= 0 && direction[prev] == 0 && gear[start][6] != gear[prev][2]) {
+		fillDirection(prev, rotateDirection * -1);
 	}
 }
 
-void rotation() {
+void rotateGear() {
 	for (int i = 0; i < 4; i++) {
-		if (direct[i] == 1) {//시계방향
-			int temp = cgear[i][7];
-			for (int j = 7; j > 0; j--) {
-				cgear[i][j] = cgear[i][j - 1];
-			}
-			cgear[i][0] = temp;
+		if (direction[i] == 1) {//시계방향
+			int temp = gear[i][7];
+			for (int j = 7; j > 0; j--)
+				gear[i][j] = gear[i][j - 1];
+			gear[i][0] = temp;
 		}
-		else if (direct[i] == -1) {//반시계방향
-			int temp = cgear[i][0];
-			for (int j = 0; j <7; j++) {
-				cgear[i][j] = cgear[i][j + 1];
-			}
-			cgear[i][7] = temp;
+		else if (direction[i] == -1) {//반시계방향
+			int temp = gear[i][0];
+			for (int j = 0; j <7; j++)
+				gear[i][j] = gear[i][j + 1];
+			gear[i][7] = temp;
 		}
 	}
 }
 
-void directClear() {
+void clearDirection() {
 	for (int i = 0; i < 4; i++) {
-		direct[i] = 0;
+		direction[i] = 0;
 	}
 }
 
-int solution(vector<vector<int>> gear, int K, vector<vector<int>>rotate) {
-	cgear = gear;
-	for (int i = 0; i < K; i++) {
-		int rotateIndex = rotate[i][0] - 1;
-		int direction = rotate[i][1];
-		fillDirect(rotateIndex, direction);
-		rotation();
-		directClear();
-	}
-	
-	int score = 0;
-	for (int j = 0; j < 4; j++) {
-		if (cgear[j][0] == 1) {
-			score += (int)pow(2, j);
-		}
-	}
-	return score;
-
+int getPoint(){
+    int point = 0;
+    for(int i =0; i < 4; i++){
+        if(gear[i][0]) point += pow(2,i);
+    }
+    return point;
 }
 
 int main() {
-	string gear;
-	vector<int> gearInside;
-	vector<vector<int>>gearOutside;
-	for (int i = 0; i < 4; i++) {
-		cin >> gear;
-		for (int j = 0; j < 8; j++) {
-			gearInside.push_back(gear[j]-'0');
-		}
-		gearOutside.push_back(gearInside);
-		gearInside.clear();
-	}
-	int K;
-	cin >> K;
-	vector<int> rotateIn;
-	vector<vector<int>>rotateOut;
 	int gearNum;
-	int direct;
-	for (int i = 0; i < K; i++) {
-			cin >> gearNum;
-			rotateIn.push_back(gearNum);
-			cin >> direct;
-			rotateIn.push_back(direct);
-			rotateOut.push_back(rotateIn);
-			rotateIn.clear();
+	int rotateDirection;
+	int k;
+
+	for (int i = 0; i < 4; i++) {
+    	string gearInfo;
+		cin >> gearInfo;
+		gear.push_back(getGearVector(gearInfo));
 	}
-	int ans = solution(gearOutside, K, rotateOut);
-	cout << ans;
+
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> gearNum >> rotateDirection;
+		fillDirection(gearNum-1, rotateDirection);
+		rotateGear();
+		clearDirection();
+	}
+
+	cout << getPoint();
 }
