32e67db339dd01def52ddfe949462b730cf93676
zmrdltl <meenseek5929@naver.com>
2021-01-13 19:00:54 +0900

    https://www.acmicpc.net/problem/1972

--- boj/recursion/1074.cpp
+++ boj/recursion/1074.cpp
@@ -1,66 +1,23 @@
-// #include <iostream>
-// #include <cmath>
-// using namespace std;
-
-// int n,r,c,ans=0;
-
-// void fillBoard(int n,int x,int y){
-//     if(x== r && y == c){
-//         cout << ans;
-//         return;
-//     }
-//     if (y <= r && r < y + n && x <= c && c < x + n && ){
-//         fillBoard(n/2,x,y);
-//         fillBoard(n/2,x,y+n/2);
-//         fillBoard(n/2,x+n/2,y);
-//         fillBoard(n/2,x+n/2,y+n/2);
-//     }
-//     ans += n*n;
-// }
-
-// int main(){
-//     cin >> n >> r >> c; // 2*n X 2*n의 판에서 (r,c)는 무슨 숫자?
-//     fillBoard(1<<n,0,0);
-// }
-
 #include <iostream>
-
 using namespace std;
 
-int n, r, c;
-int ans;
+int n,r,c,ans=0;
 
-void Z(int y, int x, int size)
-{
-    if (y == r && x == c)
-    {
-        cout << ans << '\n';
+void fillBoard(int n,int x,int y){
+    if(x == c && y == r){
+        cout << ans;
         return;
     }
-
-    // r,c가 현재 사분면에 존재한다면
-    if (r < y + size && r >= y && c < x + size && c >= x)
-    {
-        // 1사분면 탐색
-        Z(y, x, size / 2);
-        // 2사분면 탐색
-        Z(y, x + size / 2, size / 2);
-        // 3사분면 탐색
-        Z(y + size / 2, x, size / 2);
-        // 4사분면 탐색
-        Z(y + size / 2, x + size / 2, size / 2);
+    if (y <= r && r < y + n && x <= c && c < x + n){
+        fillBoard(n/2,x,y);
+        fillBoard(n/2,x+n/2,y);
+        fillBoard(n/2,x,y+n/2);
+        fillBoard(n/2,x+n/2,y+n/2);
     }
-    else
-    {
-        ans += size * size;
-    }
+    else ans += n*n;
 }
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
 
-    cin >> n >> r >> c;
-    Z(0, 0, (1 << n));
-    return 0;
+int main(){
+    cin >> n >> r >> c; // 2*n X 2*n의 판에서 (r,c)는 무슨 숫자?
+    fillBoard(1<<n,0,0);
 }
---
+++ boj/recursion/1972.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int check[100];
+map<string,int> m;
+vector<string> cnt[100];
+string word;
+
+bool isSuprising(){
+    for(int i =1 ; i <= word.size(); i++){
+        m.clear();
+        for(int j = 0; j < cnt[i].size(); j++){
+            if(m[cnt[i][j]]) return 0;
+            else m[cnt[i][j]]++;
+        }
+    }
+    return 1;
+}
+
+void backtracking(string word, string w, int idx, int level,int source){
+    if(level==2){
+        cnt[abs(idx-source)].push_back(w);
+        return;
+    }
+    for(int i = idx; i < word.size(); i++){
+        if(!check[i]){
+            check[i] = 1;
+            backtracking(word,w+word[i],i,level+1,idx);
+            check[i] = 0;
+        }
+    }
+
+}
+
+int main(){
+    while(1){
+        cin >> word;
+        memset(check,0,sizeof(check));
+        memset(cnt,0,sizeof(cnt));
+        m.clear();
+        if(word=="*")break;
+
+        backtracking(word,"",0,0,0);
+        if(isSuprising()){
+            cout << word << " is surprising.\n";
+        }else{
+            cout << word << " is NOT surprising.\n";
+        }
+    }
+}
