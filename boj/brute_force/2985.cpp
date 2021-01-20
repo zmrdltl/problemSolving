49b9cebbc9d574ba676ef4aeae8f86e08f6c65f9
zmrdltl <meenseek5929@naver.com>
2021-01-20 18:34:02 +0900

    https://www.acmicpc.net/problem/2985

---
+++ boj/brute_force/2985.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double a,b,c;
+    cin >> a >> b >> c;
+    if(a + b == c)
+        cout << a << '+' << b << '=' <<c;
+    else if(a - b == c)
+        cout << a << '-' << b << '=' <<c;
+    else if(a * b == c)
+        cout << a << '*' << b << '=' <<c;
+    else if(a / b == c)
+        cout << a << '/' << b << '=' <<c;
+    else if(a == b + c)
+        cout << a << '=' << b << '+' <<c;
+    else if(a == b - c)
+        cout << a << '=' << b << '-' <<c;
+    else if(a == b * c)
+        cout << a << '=' << b << '*' <<c;
+    else if(a == b / c)
+        cout << a << '=' << b << '/' <<c;
+}
