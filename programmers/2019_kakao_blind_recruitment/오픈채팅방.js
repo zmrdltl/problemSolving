45d0ae54b3874f5dc7c20aef8ffa94b824ce6dff
zmrdltl <meenseek5929@naver.com>
2021-01-26 21:00:14 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42888

---
+++ programmers/2019_kakao_blind_recruitment/오픈채팅방.js
@@ -0,0 +1,23 @@
+function solution(record) {
+  var answer = [];
+  const map = new Map();
+  for (let i = 0; i < record.length; i++) {
+    record[i] = record[i].split(" ");
+    const stat = record[i][0];
+    const id = record[i][1];
+    const nickName = record[i][2];
+    if (nickName) map.set(id, nickName);
+  }
+
+  for (let i = 0; i < record.length; i++) {
+    const stat = record[i][0];
+    const id = record[i][1];
+
+    if (stat === "Enter") {
+      answer.push(map.get(id) + "님이 들어왔습니다.");
+    } else if (stat === "Leave") {
+      answer.push(map.get(id) + "님이 나갔습니다.");
+    }
+  }
+  return answer;
+}
