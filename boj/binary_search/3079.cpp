69f729b9fbbd48efd33ffba6be021f957a2914cb
zmrdltl <meenseek5929@naver.com>
2021-01-26 16:46:10 +0900

    https://www.acmicpc.net/problem/3079

--- boj/binary_search/3079.cpp
+++ boj/binary_search/3079.cpp
@@ -1,34 +1,23 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 #define ll long long
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-ll n,m;
+ll n, m, MAX;
 ll times[100001];
-ll MAX;
 ll binarySearch(){
     ll ansTime = MAX * m;
-
     ll l = 1;
     ll r = MAX * m;
     while(l<=r){
-        ll mid = (l+r)/2;
-        ll sum  =0;
-        for(ll i = 0; i < n; i++){
-            sum += mid / times[i];
-        }
-        if(sum >= m){
-            ansTime = min(ansTime,mid);
-            r = mid - 1;
-        }
-        else{
-            l = mid + 1;
-        }
+        ll mid = (l + r) / 2;
+        ll sum = 0;
+        for(ll i = 0; i < n; i++) sum += mid / times[i];
+        if(sum >= m) r = mid - 1, ansTime = min(ansTime, mid);
+        else l = mid + 1;
     }
     return ansTime;
 }
+
 int main(){
-    fastio;
     cin >> n >> m;
     for(ll i = 0; i < n; i++){
         cin >> times[i];
