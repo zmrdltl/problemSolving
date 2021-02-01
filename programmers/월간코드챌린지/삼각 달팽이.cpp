9fd515bf0e8fdfdfdc3887c52b74030f9fdc8876
zmrdltl <meenseek5929@naver.com>
2021-02-01 16:36:49 +0900

    https://programmers.co.kr/learn/courses/30/lessons/68645?language=cpp

--- programmers/월간코드챌린지/삼각 달팽이.cpp
+++ programmers/월간코드챌린지/삼각 달팽이.cpp
@@ -3,43 +3,35 @@ using namespace std;
 
 vector<int> solution(int n) {
     vector<int> answer;
-    int snail[1001][1001] = {0, };
+    int snail[1001][1001];
     int x = 0, y = 0, state = 0, num = 1;
     for(int i = 0; i < n; i++){
-        cout << "x , y : "<< x << ' ' << y <<'\n';
         if(state == 0){
-            
-            for(int j = i; j < n - 1; j++){
+            for(int j = i; j < n; j++)
                 snail[x++][y] = num++;
-            }
-            
+            x--; y++;
             state = 1;
             continue;
         }
+        
         if(state == 1){
-            
-            for(int j = i; j < n; j++){
+            for(int j = i; j < n; j++)
                 snail[x][y++] = num++;
-            }
+            x--; y-=2;
             state = 2;
             continue;
-            
-        }else{
-            for(int j = i; j < n; j++){
+        }
+        
+        if(state == 2){
+            for(int j = i; j < n; j++)
                 snail[x--][y--] = num++;
-            }
+            x+=2; y++;
             state = 0;
         }
     }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(snail[i][j]){
-                answer.push_back(snail[i][j]);
-            }
-        }
-    }
-    for(int i = 0; i < answer.size(); i++){
-        cout << answer[i] << ' ';
-    }
+    
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(snail[i][j]) answer.push_back(snail[i][j]);
     return answer;
 }
