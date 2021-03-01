2b409a92e0dc2a4deeabe885f6e45ef44e42d734
zmrdltl <meenseek5929@naver.com>
2021-03-01 16:54:16 +0900

    2021.03.01

---
+++ programmers/연습문제/수박수박수박수박수박수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for(int i = 0; i < n; i++){
+        if(i%2==0) answer += "¼ö";
+        else answer += "¹Ú";
+    }
+    return answer;
+}
