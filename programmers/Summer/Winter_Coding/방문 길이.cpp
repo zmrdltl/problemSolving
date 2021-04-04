ac1064197afdd90c59b21f1958ea4814bf56e9f9
zmrdltl <meenseek5929@naver.com>
2021-04-04 23:28:43 +0900

    2021.04.04

--- programmers/Summer/Winter_Coding/방문 길이.cpp
+++ programmers/Summer/Winter_Coding/방문 길이.cpp
@@ -13,9 +13,9 @@ int solution(string dirs) {
         if(dirs[i] == 'U'){
             if(-5 <= curY + 1 && curY + 1 <= 5){
                 nextY = curY + 1;
-                if(!ck[curY][curX][0]){
-                    ck[curY][curX][0] = 1;
-                    ck[nextY][nextX][1] = 1;
+                if(!ck[curY+5][curX+5][0]){
+                    ck[curY+5][curX+5][0] = 1;
+                    ck[nextY+5][nextX+5][1] = 1;
                     answer++;
                 }
 
@@ -24,9 +24,9 @@ int solution(string dirs) {
         else if(dirs[i] == 'D'){
             if(-5 <= curY - 1 && curY - 1 <= 5){
                 nextY = curY - 1;
-                if(!ck[curY][curX][1]){
-                    ck[curY][curX][1] = 1;
-                    ck[nextY][nextX][0] = 1;
+                if(!ck[curY+5][curX+5][1]){
+                    ck[curY+5][curX+5][1] = 1;
+                    ck[nextY+5][nextX+5][0] = 1;
                     answer++;
                 }
 
@@ -35,11 +35,10 @@ int solution(string dirs) {
 
         else if(dirs[i] == 'L'){
             if(-5 <= curX - 1 && curX - 1 <= 5){
-
                 nextX = curX - 1;
-                if(!ck[curY][curX][2]){
-                    ck[curY][curX][2] = 1;
-                    ck[nextY][nextX][3] = 1;
+                if(!ck[curY+5][curX+5][2]){
+                    ck[curY+5][curX+5][2] = 1;
+                    ck[nextY+5][nextX+5][3] = 1;
                     answer++;
                 }
             }
@@ -47,16 +46,15 @@ int solution(string dirs) {
         else{
             if(-5 <= curX + 1 && curX + 1 <= 5){
                 nextX = curX + 1;
-                if(!ck[curY][curX][3]){
-                    ck[curY][curX][3] = 1;
-                    ck[nextY][nextX][2] = 1;
+                if(!ck[curY+5][curX+5][3]){
+                    ck[curY+5][curX+5][3] = 1;
+                    ck[nextY+5][nextX+5][2] = 1;
                     answer++;
                 }
             }
         }
         curX = nextX;
         curY = nextY;
-        cout << curX << ' ' << curY << ' ' << answer<<'\n';
     }
     return answer;
 }
