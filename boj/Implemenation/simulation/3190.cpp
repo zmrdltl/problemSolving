3df067dfeb2ed20a4d31e8afa9c73e15ad0cf7c2
zmrdltl <meenseek5929@naver.com>
2021-04-09 19:48:59 +0900

    2021.04.09

--- boj/Implemenation/simulation/3190.cpp
+++ boj/Implemenation/simulation/3190.cpp
@@ -6,57 +6,47 @@ int board[101][101];
 int dx[] = {-1,0,1,0};
 int dy[] = {0,1,0,-1};
 
-int n,k,l,ans,flag,direction = 1;
+int n,k,l,sec,flag,direction = 1;
 
 struct Snake {int x,y;};
-struct Info {int x, dir;};
 
 deque <Snake> snake;
-queue <Info> q;
-
-void goSnake(int start, int end){
-    for(int time = start; time <= end; time++){
-        ans++;
-        int x = snake.front().x;
-        int y = snake.front().y;
-        int nx = x + dx[direction];
-        int ny = y + dy[direction];
-        for(auto &el : snake){
-            if(el.x == nx && el.y == ny) { flag = 1; break;}
-        }
-        if(1 > nx || nx > n || 1 > ny || ny > n || flag){ flag = 1; break; }
-        snake.push_front({nx,ny});
-        if(!board[nx][ny]){ snake.pop_back();}
-        else board[nx][ny] = 0;
-    }
-}
+map <int,char> m;
 
 int main(){
     snake.push_back({1,1});
     cin >> n >> k;
+
     while(k--){
         int x,y;
         cin >> x >> y;
         board[x][y] = 1;
     }
     cin >> l;
+
     while(l--){
         int x;
         char dir;
         cin >> x >> dir;
-        q.push({x,dir});
+        m[x] = dir;
     }
 
-    while(!q.empty()){
-        int x = q.front().x;
-        int dir = q.front().dir;
-        goSnake(ans + 1, x);
-        if(flag) break;
-        ans = x;
-        if(dir == 'D') direction = (direction + 1) % 4;
-        else direction = (3 + direction) % 4;
-        q.pop();
+    while(1){
+        sec++;
+        int x = snake.front().x;
+        int y = snake.front().y;
+        int nx = x + dx[direction];
+        int ny = y + dy[direction];
+        for(auto &el : snake)
+            if(el.x == nx && el.y == ny) 
+                { flag = 1; break; }
+        if(1 > nx || nx > n || 1 > ny || ny > n || flag) { flag = 1; break; }
+        snake.push_front({nx,ny});
+        if(!board[nx][ny]){ snake.pop_back();}
+        else board[nx][ny] = 0;
+        if(m[sec] == 'D') direction = (direction + 1) % 4;
+        else if(m[sec] == 'L') direction = (3 + direction) % 4;
     }
-    if(!flag){ goSnake(ans,20000); }
-    cout << ans << '\n';
+
+    cout << sec << '\n';
 }
