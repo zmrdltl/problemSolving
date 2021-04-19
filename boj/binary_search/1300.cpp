7ff139ab679234c1f374298191df980463466b6b
zmrdltl <meenseek5929@naver.com>
2021-04-19 14:08:46 +0900

    2021.04.19

--- boj/binary_search/1300.cpp
+++ boj/binary_search/1300.cpp
@@ -1,8 +1,7 @@
-//TODO 미구현
 #include <iostream>
 using namespace std;
 int main(){
     int n,k;
     cin >> n >> k;
-
+    
 }
---
+++ boj/string/14405.py
@@ -0,0 +1,6 @@
+s = input()
+a = s.count("pi") * 2
+b = s.count("ka") * 2
+c = s.count("chu") * 3
+if (a+b+c is len(s)) : print("YES")
+else : print("NO")
