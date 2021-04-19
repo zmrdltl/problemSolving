6919dbb01e7c24628f4576acb6077f47e8adbb67
zmrdltl <meenseek5929@naver.com>
2021-04-19 18:55:28 +0900

    2021.04.19

--- boj/graph/floyd/11404.cpp
+++ boj/graph/floyd/11404.cpp
@@ -20,7 +20,6 @@ int main(){
     for(int k = 1; k <= n; k++){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                if(dist[i][k] == INF || dist[k][j] == INF) continue;
                 if(dist[i][k] + dist[k][j] < dist[i][j]) {
                     dist[i][j] = dist[i][k] + dist[k][j];
                 }
