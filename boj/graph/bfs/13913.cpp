2a223f812c69fd79eb907b68765a976ad8110b37
zmrdltl <meenseek5929@naver.com>
2021-04-22 17:42:51 +0900

    2021.04.22

--- boj/graph/bfs/13913.cpp
+++ boj/graph/bfs/13913.cpp
@@ -1,15 +1,11 @@
 #include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define INF 0x3f3f3f3f
-
 using namespace std;
 using pii = pair<int,int>;
 int ck[100001], n, k;
+vector <int> vec;
 
-int main(){
-    fastio;
-    memset(ck,-1,sizeof(ck));
-    cin >> n >> k;
+int bfs(){
     queue<pii> q;
     q.push({n,0});
     ck[n] = 1;
@@ -19,10 +15,7 @@ int main(){
         int sec = q.front().second;
         q.pop();
         
-        if(pos == k){
-            cout << sec << endl;
-            break;
-        }
+        if(pos == k) return sec;
         
         if( pos + 1 < 100001 && ck[pos+1] == -1){
             ck[pos+1] = pos;
@@ -39,14 +32,18 @@ int main(){
             q.push({pos*2, sec+1});
         }
     }
+}
+int main(){
+    fastio;
+    memset(ck,-1,sizeof(ck));
+    cin >> n >> k;
     
-    vector<int> vec;
+    cout << bfs() << '\n';
     vec.push_back(k);
+
     while (k != n) {
         vec.push_back(ck[k]);
         k = ck[k];
     }
-    
-    for(int i = vec.size()-1; i>=0; i--)
-        cout << vec[i] << " ";
+    for(int i = vec.size() - 1; i >= 0; i--) cout << vec[i] << " ";
 }
