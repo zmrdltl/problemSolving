48bb6e2d2dedeb9cbd19cfaad30d7e18ce156cdb
zmrdltl <meenseek5929@naver.com>
2021-04-25 18:37:02 +0900

    2021.04.25

--- boj/ds/11066.cpp
+++ boj/ds/11066.cpp
@@ -1,23 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int t, n;
-int paper[501];
-int d[501][10001];
-
-int dp(int n, int m){
-
-    int &ret = d[n][m];
+int d[501][501];
+int cost[501];
+int dp(int i, int j){
+    if(i == j) return 0;
+    int &ret = d[i][j];
     if(ret != -1) return ret;
-    
+    ret = 0x3f3f3f3f;
+    for(int x = i; x < j; x++){
+        ret = min(ret, dp(i,x) + dp(x+1,j) + cost[j] - cost[i-1]);
+    }
     return ret;
 }
 
 int main(){
     cin >> t;
+
     while(t--){
         cin >> n;
         memset(d,-1,sizeof(d));
-        for(int i = 0; i < n; i++) cin >> paper[i];
-
+        for(int i = 1; i <= n; i++) cin >> cost[i], cost[i] += cost[i-1];
+        cout << dp(1,n) << '\n';
     }
 }
