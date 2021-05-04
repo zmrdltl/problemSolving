151c29e222ec81b27680ab27b34cccc1121c444e
zmrdltl <meenseek5929@naver.com>
2021-05-04 17:39:43 +0900

    2021.05.04

--- programmers/2018_kakao_blind_recruitment/[3차] 압축.cpp
+++ programmers/2018_kakao_blind_recruitment/[3차] 압축.cpp
@@ -4,8 +4,26 @@ using namespace std;
 vector<int> solution(string msg) {
     vector<int> answer;
     map <string,int> m;
-    for(int i = 0; i < msg.size(); i++){
-        
+    int cnt = 0, piv = 0;
+    string w ="";
+    for(int i = 0; i < 26; i++){
+        string s(1,'A' + i);
+        m[s] = ++cnt;
     }
+    for(int i = 0; i < msg.size(); i++){
+        string w(1,msg[piv]);
+        for(int j = piv + 1; j < msg.size(); j++){
+            if(m[w + msg[j]]) w += msg[j];
+            else{
+                answer.push_back(m[w]);
+                piv = j;
+                m[w+msg[j]] = ++cnt;
+                break;
+            }
+        }
+    }
+    string surplus = msg.substr(piv);
+    if(m[surplus]) answer.push_back(m[surplus]);
+    else answer.push_back(++cnt);
     return answer;
 }
