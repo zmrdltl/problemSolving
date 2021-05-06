a0353772220cf7c9167d8752bd7cc85376d7029c
zmrdltl <meenseek5929@naver.com>
2021-05-06 16:30:40 +0900

    2021.05.06

---
+++ boj/Implemenation/17413.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s, ans;
+stack <char> st;
+int main(){
+    getline(cin,s);
+    int flag = 0;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] != '<') st.push(s[i]);
+        if(s[i] == '<') {
+            string tmp;
+            while(!st.empty()){
+                tmp += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+            ans += tmp;
+            flag = 1;
+        }
+        else if(s[i] == '>'){
+            string tmp;
+            while(!st.empty()){
+                tmp += st.top();
+                st.pop();
+            }
+            if(flag) {
+                flag = 0;
+                reverse(tmp.begin(),tmp.end());
+                ans += tmp;
+            }
+        }
+        else if(s[i] == ' ' || i == s.size() - 1){
+            if(!flag){
+                int f = 0;
+                string tmp;
+                while(!st.empty()){
+                    if(st.top()!= ' ') tmp += st.top();
+                    else f = 1;
+                    st.pop();
+                }
+                ans += tmp;
+                if(f) ans += ' ';
+            }    
+        }
+    }
+    cout << ans << '\n';
+}
