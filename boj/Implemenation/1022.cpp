606b2a5086afa0e38ef747c9d1071f00cacc6b02
zmrdltl <meenseek5929@naver.com>
2021-05-09 23:21:35 +0900

    2021.05.09

--- boj/Implemenation/1022.cpp
+++ boj/Implemenation/1022.cpp
@@ -4,20 +4,19 @@ int dx[4] = { 0,-1,0,1 };
 int dy[4] = { -1,0,1,0 };
 int board[50][5], curX, curY, dir = 3;
 int cnt, num = 1, dcnt = 1;
+int r1, c1, r2, c2;
+
+bool isOk(){
+    if(!board[r2 - r1][0] || !board[0][0] || !board[0][c2 - c1] || !board[r2 - r1][c2 - c1]) return true;
+    return false;
+}
 
 int main(){
-	int r1, c1, r2, c2;
 	cin >> r1 >> c1 >> r2 >> c2;
 
-	while (!board[r2 - r1][0] || !board[0][0] || !board[0][c2 - c1] || !board[r2 - r1][c2 - c1]) {
-
-		if (curY - r1 >= 0 
-        && curY - r1 <= (r2 - r1) 
-        && curX - c1 >= 0 
-        && curX - c1 <= (c2 - c1)) {
+	while (isOk()) {
+		if (curY - r1 >= 0 && curY - r1 <= (r2 - r1) && curX - c1 >= 0 && curX - c1 <= (c2 - c1))
 			board[curY - r1][curX - c1] = num;
-		}
-
 		cnt++;
 		num++;
 
@@ -26,12 +25,10 @@ int main(){
 
 		if (cnt == dcnt) {
 			cnt = 0;
-			dir = (dir + 1) % 4;
-			if (dir == 1 || dir == 3)
-				dcnt += 1;
+			dir = (dir + 1) % 4; 
+            if (dir == 1 || dir == 3) dcnt += 1;
 		}
 	}
-
 
 	cnt = 0;
 
--- programmers/2017_kakao_finals/튜브의 소개팅.cpp
+++ programmers/2017_kakao_finals/튜브의 소개팅.cpp
@@ -1,88 +1,58 @@
-//TODO
 #include <bits/stdc++.h>
 #define ll long long
 #define INF 0x3f3f3f3f3f3f3f3f
 using namespace std;
-using pii = pair<int,int>;
-using tpll = tuple<ll,ll,ll>;
+using pllll = tuple<ll,ll,ll,ll>;
 int dx[] = {0,0,-1,1};
 int dy[] = {-1,1,0,0};
+int M, N, S;
+ll speakTime[51][51];
 
-int m, n, s;
-int ck[51][51];
 vector<vector<int>> board;
-vector <int> graph[51];
-ll dist[51][51];
 
-void bfs(){
-    queue <pii> q;
-    q.push({0,0});
-    ck[0][0] = 1;
-    while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(0 > nx || nx >= m || 0 > ny || ny >= n) continue;
-            if(board[nx][ny] == -1 || ck[nx][ny]) continue;
-            ck[nx][ny] = 1;
-            graph[nx].push_back(ny);
-            q.push({nx,ny});
-        }
-    }
-    for(int i = 0; i < 5; i++){
-        if(graph[i].size()){
-            for(auto a : graph[i])
-                cout << a << ' ';
-            cout << '\n';
-        }
-    }
-    cout << '\n';
-}
-void dijsktra(){
-    dist[0][0] = 0;
-    priority_queue <tpll,vector<tpll>,greater<tpll>> pq;
-    pq.push({0,0,0});
+vector <int> dijsktra(){
+    vector <ll> answer = {INF,INF};
+    speakTime[0][0] = 0;
+    priority_queue <pllll,vector<pllll>,greater<pllll>> pq;
+    //dist, s, x, y
+    pq.push({0,0,0,0});
     while(!pq.empty()){
-        int cost = get<0>(pq.top());
-        int x = get<1>(pq.top());
-        int y = get<2>(pq.top());
+        ll dist = get<0>(pq.top());
+        ll cost = get<1>(pq.top());
+        int x = get<2>(pq.top());
+        int y = get<3>(pq.top());
+
         pq.pop();
+
         for(int dir = 0; dir < 4; dir++){
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            int nextCost = board[nx][ny];
-            if(0 > nx || nx >= m || 0 > ny || ny >= n) continue;
-            cout << "x,y -> nx,ny : " << x << ' '<< y << ' ' << nx << ' ' << ny << '\n';
-            if(board[nx][ny] == -1) continue;
+            if(0 > nx || nx >= M || 0 > ny || ny >= N) continue;
 
-            if(cost + nextCost < dist[nx][ny]){
-                dist[nx][ny] = cost + nextCost;
-                pq.push({dist[nx][ny],nx,ny});
+            ll nextCost = board[nx][ny];
+
+            if(board[nx][ny] == -1 || cost + nextCost > S) continue;
+
+            if(cost + nextCost < speakTime[nx][ny]){
+                speakTime[nx][ny] = cost + nextCost;
+                pq.push({dist+1,speakTime[nx][ny],nx,ny});
+                if(nx != M - 1 || ny != N - 1) continue;
+                if(answer[0] < dist + 1) continue;
+                answer[0] = dist+1;
+                answer[1] = speakTime[nx][ny];
             }
         }
     }
+
+    int a = answer[0];
+    int b = answer[1];
+
+    return {a,b};
 }
 
 vector<int> solution(int m, int n, int s, vector<vector<int>> time_map) {
-    m = m;
-    n = n; 
-    s = s;
-    board = time_map;
-    memset(dist,INF,sizeof(dist));
-    memset(ck,0,sizeof(ck));
-    for(int i = 0; i <= 50; i++) graph[i].clear();
-    int move_distance = 0;
-    int sum_of_talk_time = 0;
-    
-    vector<int> answer(2);
-    bfs();
-    dijsktra();
-    cout << dist[m-1][n-1] << '\n';
-    answer[0] = move_distance;
-    answer[1] = sum_of_talk_time;
-    
-    return answer;
+    M = m, N = n, S = s, board = time_map;
+    for(int i = 0; i <= 50; i++)
+        for(int j = 0; j <= 50; j++) speakTime[i][j] = INF;
+    return dijsktra();
 }
