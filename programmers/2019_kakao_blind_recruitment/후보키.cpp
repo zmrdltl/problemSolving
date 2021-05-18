93e13af333970e54142543ecab6f2f464801b02b
zmrdltl <meenseek5929@naver.com>
2021-05-18 11:34:12 +0900

    2021.05.18

--- programmers/2019_kakao_blind_recruitment/후보키.cpp
+++ programmers/2019_kakao_blind_recruitment/후보키.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isMinimal(vector<int> vec,int now){
-    for(int i = 0; i < vec.size(); i++)
-        if((vec[i] & now) == vec[i]) return false;
+bool isMinimal(vector<int>ans,int col){
+    for(int i = 0; i < ans.size(); i++){
+        if((ans[i] & col) == ans[i]) return false;
+    }
     return true;
 }
-
 int solution(vector<vector<string>> relation) {
-    int rowSize = relation.size(); 
+    int rowSize= relation.size();
     int colSize = relation[0].size();
-    vector<int> ans;
+    vector <int> ans;
     for(int i = 1; i < (1<<colSize); i++){
-        set<string> s; //
+        set <string> s;
         for(int j = 0; j < rowSize; j++){
-            string now = "";
+            string data;
             for(int k = 0; k < colSize; k++){
-                // 선택된 컬럼과 일치한다면
-                if(i & (1<<k))
-                    now += relation[j][k];
+                if(i & (1 << k)) //선택 조합이라면
+                    data += relation[j][k];
             }
-            s.insert(now);
+            s.insert(data);
         }
+        
         if(s.size() == rowSize && isMinimal(ans,i))
             ans.push_back(i);
     }
