59eabc22717fb7afb6ffa928af2ce8b45d1dbe54
zmrdltl <meenseek5929@naver.com>
2021-05-24 15:15:18 +0900

    2021.05.23

---
+++ boj/graph/tree/1135.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int,int>;
+int n, maxDepth;
+vector <vector<pii>> graph;
+//graph[a][b] : a를 부모로, b를 자식으로 했을 때 {최댓값과,자식노드}로 구성
+
+int dfs(int cur){
+    int ret = 0;
+    for(auto &next : graph[cur]){
+        next.first = 1 + dfs(next.second);
+    }
+    sort(graph[cur].rbegin(), graph[cur].rend());
+    for(int i = 0; i < graph[cur].size(); i++){
+        ret = max(ret, graph[cur][i].first + i);
+    }
+    
+    return ret;
+}
+int main(){
+    int node;
+    cin >> n >> node;
+    graph = vector<vector<pii>>(n);
+
+    for(int i = 1; i < n; i++){
+        cin >> node;
+        graph[node].push_back({0,i});
+    }
+    cout << dfs(0);
+}
