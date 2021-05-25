65ddbb42cd7b91a37ba3521b98fea2e834a4e5d3
zmrdltl <meenseek5929@naver.com>
2021-05-25 20:21:54 +0900

    2021.05.25

--- boj/graph/bfs/2665.cpp
+++ boj/graph/bfs/2665.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using pii = pair<int,int>;
 int n;
-int ck[51][51], board[51][51], dist[51][51]; //마지막 0은 부순상태, 안부순 상태
+int ck[51][51], board[51][51]; //마지막 0은 부순상태, 안부순 상태
 int dx[] = {0,0,-1,1};
 int dy[] = {-1,1,0,0};
 
@@ -11,8 +11,6 @@ int bfs(){
     queue <pii> q;
     q.push({0,0});
     ck[0][0] = 0;
-    int ans = 0x3f3f3f3f;
-    int cnt = 0;
     while(!q.empty()){
         int x = q.front().first;
         int y = q.front().second;
@@ -21,7 +19,6 @@ int bfs(){
             int nx = x + dx[i];
             int ny = y + dy[i];
             if(0 > nx || nx >= n || 0 > ny || ny >= n) continue;
-            if(!ck[nx][ny]) continue;
             if(!board[nx][ny]){
                 if(ck[nx][ny] == -1 || ck[nx][ny] > ck[x][y] + 1){
                     ck[nx][ny] = ck[x][y] + 1;
@@ -38,13 +35,14 @@ int bfs(){
     }
     return ck[n-1][n-1];
 }
+
 int main(){
+    memset(ck,-1,sizeof(ck));
     cin >> n;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             scanf("%1d",&board[i][j]);
         }
     }
-    memset(ck,-1,sizeof(ck));
     cout << bfs() << '\n';
 }
