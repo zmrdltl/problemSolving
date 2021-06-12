c9f2cfb5310d493ce5b52a9ec3114dcc40ac9685
zmrdltl <meenseek5929@naver.com>
2021-06-12 19:54:17 +0900

    2021.05.12

---
+++ programmers/2021_kakao_blind_recruitment/합승 택시 요금.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define INF 1000000000
+using namespace std;
+int charge[201][201], ans, N;
+
+void initCharge(){
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            charge[i][j] = INF;
+        }
+    }
+}
+
+void makeChargeMatrix(vector<vector<int>> fares){
+    for(int i = 0; i < fares.size(); i++){
+        charge[fares[i][0]][fares[i][1]] = fares[i][2];
+        charge[fares[i][1]][fares[i][0]] = fares[i][2];
+    }
+    for(int i = 1; i <= N; i++) charge[i][i] = 0;
+}
+
+void makeFloyd(){
+    for(int k = 1; k <= N; k++){
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                if(i == j || j == k || i == k) continue;
+                if(charge[i][k] == INF || charge[k][j] == INF) continue;             
+                charge[i][j] = min(charge[i][j],charge[i][k] + charge[k][j]);
+            }
+        }
+    }
+}
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    N = n;
+    initCharge();
+    makeChargeMatrix(fares);
+    makeFloyd();
+    ans = charge[s][a] + charge[s][b];
+
+    for(int i = 1; i <= N; i++){
+        //n까지 동승해서 흩어지는 것이 기존 따로 가는 것보다 작다면
+        if(charge[s][i] == INF || charge[i][a] == INF || charge[i][b] == INF) continue;
+        ans = min(ans,charge[s][i] + charge[i][a] + charge[i][b]);
+    }
+    return ans;
+}
