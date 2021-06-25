44e7b4e5376187a5a1c0256a95b11ce8f57d92bc
zmrdltl <meenseek5929@naver.com>
2021-06-25 22:20:30 +0900

    2021.06.25

--- programmers/2020_kakao_blind_recruitment/외벽 점검.cpp
+++ programmers/2020_kakao_blind_recruitment/외벽 점검.cpp
@@ -2,7 +2,69 @@
 #define ll long long 
 using namespace std;
 map <string,int> m;
+int distCk[10];
+int weakCk[201];
 
-int solution(vector<int> &A) {
-    
+bool isAllCovered(vector<int> &weak){
+    for(auto w : weak){
+        cout << "COVER : ";
+        cout << w << ' ';
+        if(!weakCk[w]) return false;
+    }
+    return true;
+}
+int solution(int n, vector<int> weak, vector<int> dist) {
+    memset(distCk,0,sizeof(distCk));
+    memset(weakCk,0,sizeof(weakCk));
+    int ans = 0x3f3f3f3f;
+    do{
+
+        int wSize = weak.size();
+        int dSize = dist.size();
+        int cnt = 0;
+        memset(distCk,0,sizeof(distCk));
+        memset(weakCk,0,sizeof(weakCk));
+        for(int i = 0; i < wSize; i++){
+            
+            
+            int gap = weak[(i+1) % wSize] - weak[i];
+            if(i == wSize - 1) gap = weak[0] + n - weak[i];
+            
+
+            for(int j = 0; j < dSize; j++){
+                if(distCk[j]) continue;
+                if(gap <= dist[j]){
+                   // cout << "GAP : " << weak[i] << ' ' << weak[(i+1)%wSize] << ' ' << gap << '\n';
+                    
+                    if(i == wSize - 1){
+                        for(int k = weak[i]; k <= n; k++) weakCk[k] = 1;
+                        for(int k = 0; k <= weak[0]; k++) weakCk[k] = 1;
+                    }
+                    else{
+                        for(int k = weak[i]; k <= weak[i+1]; k++)
+                            weakCk[k] = 1;
+                    }
+                    
+                    distCk[j] = 1;
+                    cnt++;
+                    cout << gap << "군열 : " << weak[i] << "에 " << dist[j] << "사용" << '\n';
+                    break;
+                }
+            }
+        }
+        cout << "CNT : " << cnt << '\n';
+        cout << "WEAK : " ;
+        for(int i = 1; i <= 10; i++) cout << weakCk[i] << ' ';
+        cout << '\n';
+        cout << "DIST : ";
+        for(int i = 0; i < dSize; i++) cout << distCk[i] << ' ';
+        cout << '\n';
+        if(cnt && cnt <= dSize && isAllCovered(weak))
+            ans = min(ans,cnt);
+        cout << '\n';
+        cout << '\n';
+    }while(next_permutation(dist.begin(),dist.end()));
+
+    if(ans == 0x3f3f3f3f) return -1;
+    return ans;
 }
--- programmers/sum_win_coding(2019)/기지국 설치.cpp
+++ programmers/sum_win_coding(2019)/기지국 설치.cpp
@@ -1,30 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(int n, vector<int> stations, int w){
-    vector <int> emptySpaces;
-    int size = stations.size();
-    int ans = 0;
+// int solution(int n, vector<int> stations, int w){
+//     vector <int> emptySpaces;
+//     int size = stations.size();
+//     int ans = 0;
 
-    int space = stations[0] - w - 1;
-    if(space < 0) space = 0;
-    emptySpaces.push_back(space);
+//     int space = stations[0] - w - 1;
+//     emptySpaces.push_back(space);
 
-    for(int i = 1; i < size; i++){
-        space = stations[i] - stations[i-1] - 1 - w * 2;
-        if(space < 0) space = 0;
-        emptySpaces.push_back(space);
+//     for(int i = 1; i < size; i++){
+//         space = stations[i] - stations[i-1] - 1 - w * 2;
+//         emptySpaces.push_back(space);
+//     }
+
+//     space = n - stations[size - 1] - w;
+//     emptySpaces.push_back(space);
+
+//     int coverLength = w * 2 + 1;
+//     for(auto e : emptySpaces) {
+//         if(e <= 0) continue;
+//         if(e %  coverLength) ans += e/coverLength + 1;
+//         else ans += e/coverLength;
+//     }
+
+//     return ans;
+// }
+
+int main(){
+    while(1){
+        cout<< "sex\n";
     }
-
-    space = n - stations[size - 1] - w;
-    if(space < 0) space = 0;
-    emptySpaces.push_back(space);
-
-    int coverLength = w * 2 + 1;
-    for(auto e : emptySpaces) {
-        if(e %  coverLength) ans += e/coverLength + 1;
-        else ans += e/coverLength;
-    }
-
-    return ans;
 }
