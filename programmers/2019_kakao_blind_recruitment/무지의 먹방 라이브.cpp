f31559a68287428a13be56fb35d1f2ecd21d3370
zmrdltl <meenseek5929@naver.com>
2021-06-28 17:47:49 +0900

    2021.06.28

--- programmers/2019_kakao_blind_recruitment/무지의 먹방 라이브.cpp
+++ programmers/2019_kakao_blind_recruitment/무지의 먹방 라이브.cpp
@@ -3,45 +3,30 @@
 using namespace std;
 
 bool compare(pair<int, int> a, pair<int, int> b) {
-    return a.second < b.second;
+	return a.second < b.second;
 }
 
 int solution(vector<int> food_times, long long k) {
-    // 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
-    long long summary = 0;
-    for (int i = 0; i < food_times.size(); i++) {
-        summary += food_times[i];
-    }
-    if (summary <= k) return -1;
-    
-    // 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
-    priority_queue<pair<int, int> > pq;
-    for (int i = 0; i < food_times.size(); i++) {
-        // (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
-        pq.push({-food_times[i], i + 1});
-    }
-    
-    summary = 0; // 먹기 위해 사용한 시간
-    long long previous = 0; // 직전에 다 먹은 음식 시간
-    long long length = food_times.size(); // 남은 음식의 개수
+	vector<pair<int, int>> food_info; //<걸리는 시간, 순서>
+	long long lastTime = 0; //바로 직전 검사한 음식의 걸리는 시간
+	long long t = 0;
+	int size = food_times.size();
 
-    // summary + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
-    while (summary + ((-pq.top().first - previous) * length) <= k) {
-        int now = -pq.top().first;
-        pq.pop();
-        summary += (now - previous) * length;
-        length -= 1; // 다 먹은 음식 제외
-        previous = now; // 이전 음식 시간 재설정
-    }
+	for (int i = 0; i < food_times.size(); i++)
+		food_info.push_back(make_pair(food_times[i], i + 1));
 
-    // 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
-    vector<pair<int, int> > result;
-    while (!pq.empty()) {
-        int food_time = -pq.top().first;
-        int num = pq.top().second;
-        pq.pop();
-        result.push_back({food_time, num});
-    }
-    sort(result.begin(), result.end(), compare); // 음식의 번호 기준으로 정렬
-    return result[(k - summary) % length].second;
+	sort(food_info.begin(), food_info.end()); //소요 시간이 짧은 음식부터 검사 위함
+
+	for (vector<pair<int, int>>::iterator i = food_info.begin(); i != food_info.end(); i++, size--) {
+		t = (i -> first - lastTime) * size; 
+		if (t <= k) {
+			k -= t; //남은 시간 감소
+			lastTime = i->first;
+		}
+		else {
+			sort(i, food_info.end(), compare); //음식 번호순으로 다시 정렬
+			return (i + (k % size)) -> second;
+		}
+	}
+	return -1;
 }
