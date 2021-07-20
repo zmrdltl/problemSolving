daedf335dc3126377a99954e4f1c089f898bee01
zmrdltl <meenseek5929@naver.com>
2021-07-20 14:35:19 +0900

    2021.07.20

--- boj/binary_search/14575.cpp
+++ boj/binary_search/14575.cpp
@@ -1,37 +1,55 @@
-//TODO
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 struct Drinking { ll l, r; };
-vector <Drinking> participants(1001);
-ll totalDrink, n;
+vector <Drinking> participants;
+ll totalDrink, n, sum, maxi,mini = 0x3f3f3f3f;
 
-ll bs(){
-    ll l = 0;
-    ll r = 0x3f3f3f3f;
+//모두 S이하만큼 받으면서 알맞게 T의 술인것
+ll binarySearch(){
+    ll l = mini;
+    ll r = maxi;
+    ll ans = 0x3f3f3f3f;
+    int cnt = 0;
 
     while(l<=r){
-        ll mid = (l+r)/2;
-        ll sum = 0;
-        ll ans = 0;
-        bool cant = 0;
+        ll mid = (l + r) / 2;
+        int state = 0;
+        sum = 0;
+        cnt = 0;
         for(int i = 0; i < n; i++){
-            if(participants[i].l > mid) {cant = 1; break;}
+            //하한선이 r이라면 r을 sum에 더함
+            if(mid > participants[i].r) cnt++,sum += participants[i].r;
+            else {
+                //상한선이 l이라면 l을 sum에 더함
+                if(mid >= participants[i].l)
+                    cnt++,sum += mid;
+            }
         }
-        //cout << l << ' ' << mid << ' ' << r << '\n';
-        if(cant) l = mid + 1;
+        if(cnt < n) { l = mid + 1; continue; } 
+        if(sum < totalDrink) l = mid + 1;
+        else if (sum == totalDrink) return mid;
         else r = mid - 1;
     }
-    if(l*n == totalDrink) return l;
-    if((l-1)*(n-1) + l == totalDrink) return l;
-    if(l * n <= totalDrink) return l;
-    return -1;
+    return l;
+}
+
+bool operator < (const Drinking &a, const Drinking &b){
+    if(a.l == b.l) return a.r < b.r;
+    return a.l < b.l;
 }
 
 int main(){
     cin >> n >> totalDrink;
+    participants.resize(n);
     for(int i = 0; i < n; i++){
         cin >> participants[i].l >> participants[i].r;
+        sum += participants[i].r;
+        maxi = max(maxi, participants[i].r);
+        mini = min(mini, participants[i].l);
     }
-    cout << bs() <<'\n';
+    if(sum < totalDrink) { cout << -1; return 0; }
+    sort(participants.begin(),participants.end());
+
+    cout << binarySearch() <<'\n';
 }
