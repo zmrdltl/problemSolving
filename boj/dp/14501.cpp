c6ca83cf5c5b7a740105acce97145cd3d4b73e3f
MeenSeek Kim <meenseek5929@naver.com>
2021-08-02 14:53:56 +0900

    2021.08.02

--- boj/dp/14501.cpp
+++ boj/dp/14501.cpp
@@ -1,30 +1,26 @@
-//TODO
 #include <bits/stdc++.h>
 using namespace std;
-int d[16][2000], n;
-struct consulting{
-    int time, price;
-};
+struct Consulting { int conTime, price; };
+int n, d[16];
+vector <Consulting> c;
 
-int dp(int piv,int time,int price,vector<consulting> &v){
-    if(piv == n) return 0;
-    int & ret = d[time][price];
-    if(ret!=-1) return ret;
-    if(time - v[piv].time >= 0)
-        ret = max(ret,dp(piv+1,time - v[piv].time,price + v[piv].price,v));
-    ret = max(ret,dp(piv+1,time,price,v));
+int dp(int t){
+    if(t >= n) return 0;
+    int &ret = d[t];
+    if(ret != -1) return ret;
+    ret = 0;
+    if(t + c[t].conTime <= n)    
+        ret = max(ret, dp(t + c[t].conTime) + c[t].price);
+    ret = max(ret, dp(t+1));
     return ret;
-};
-//i번째일에서 의 금액
-int main(){
-    vector <consulting> v;
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        int t,c;
-        cin >> t >> c;
-        v.push_back({t,c});
-    }
-    memset(d,-1,sizeof(d));
-    cout << dp(0,n,0,v);
+}
 
+int main(){
+    memset(d,-1,sizeof(d));
+    cin >> n;
+    for(int i = 0,t,p; i < n; i++){
+        cin >> t >> p;
+        c.push_back({t,p});
+    }
+    cout << dp(0);
 }
