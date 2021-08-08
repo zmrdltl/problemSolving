f1a6e1db200e19571f9d759ea72786230a2e110f
MeenSeek Kim <meenseek5929@naver.com>
2021-08-08 17:44:25 +0900

    2021.08.08

--- boj/graph/segment_tree/6549.cpp
+++ boj/graph/segment_tree/6549.cpp
@@ -1,11 +1,39 @@
-//TODO
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 ll n, a[100001];
-
 int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    
+    while(1){
+        stack <ll> st;
+        ll ans = 0, left = 0;
+        cin >> n;
+        if(!n) break;
+        for(int i = 0; i < n; i++) cin >> a[i];
+        for(int i = 0; i < n; i++){
+
+            while(st.size() && a[st.top()] > a[i]){
+                ll height = a[st.top()];
+                st.pop();
+                ll width = i;
+                if(!st.empty()){
+                    width = (i - st.top() - 1);
+                }
+                ans = max(ans, height * width);
+            }
+            st.push(i);
+        }
+
+        while(st.size()){
+            ll height = a[st.top()];
+            st.pop();
+            ll width = n;
+            if(!st.empty()){
+                width = n - st.top() - 1;
+            }
+            ans = max(ans, height * width);
+        }
+
+
+        cout << ans << '\n';
+    }
 }
