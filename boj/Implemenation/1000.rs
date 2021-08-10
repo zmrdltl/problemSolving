ad0e8f72444c380c51754f5016bf32ce1f5ec97c
MeenSeek Kim <meenseek5929@naver.com>
2021-08-10 09:55:14 +0900

    2021.08.10

--- boj/Implemenation/1000.rs
+++ boj/Implemenation/1000.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello World!")
+    let mut input_number = String::new();
+
+    io::stdin().read_line(&mut input_number)
+        .expect("Falied to read line");
+
+    let numbers: Vec<&str> = input_number.split_whitespace().collect();
+
+    let number_a = match numbers[0].parse::<i32>() {
+        Ok(i) => i,
+        Err(_e) => {
+            -1
+        }
+    };
+    
+    let number_b = match numbers[1].parse::<i32>() {
+        Ok(i) => i,
+        Err(_e) => {
+            -1
+        }
+    };
+
+    println!("{}", number_a + number_b);
 }
