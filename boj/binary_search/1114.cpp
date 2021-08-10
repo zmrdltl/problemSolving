9fc47f4b17aacd638b3443377821272b3446f219
MeenSeek Kim <meenseek5929@naver.com>
2021-08-10 09:55:14 +0900

    2021.08.10

---
+++ boj/binary_search/1114.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int l, k, c;
+vector <int> posCanCut;
+int binarySearch(){
+    int left = 0;
+    int right = l;
+
+    while(left < right){
+
+    }   
+
+}
+
+int main(){
+    cin >> l >> k >> c;
+    while(k--){
+        int x;
+        cin >> x;
+        posCanCut.push_back(x);
+    }
+    cout << binarySearch();
+
+}
