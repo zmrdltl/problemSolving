e21217a83cb8e9d52fb500761e6c9a74ac338982
MeenSeek Kim <meenseek5929@naver.com>
2021-08-19 18:50:49 +0900

    2021.08.19

---
+++ boj/sweeping/2018.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,l=1,r=1,sum,ans;
+int main(){
+    cin >> n;
+    while(l <= r && r <= n){
+        if(sum < n) sum += r++;
+        else{
+            if(sum == n){ ans++;
+            sum -= l++;
+        }
+    }
+    cout << ans + 1;
+}
