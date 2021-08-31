5dea8b773a2a6afffd59cc520f39a362f4fdc99d
MeenSeek Kim <meenseek5929@naver.com>
2021-08-31 20:09:58 +0900

    2021.08.31

--- boj/graph/topology_sort/17616.cpp
+++ boj/graph/topology_sort/17616.cpp
@@ -1,34 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> pi;
-const int MAX = 100001;
-const int INF = 0x3f3f3f3f;
-int N, M, X;
-vector<int> v[2][MAX];
-bool visited[MAX];
-int dfs(int cur, int type) {
-	int ret = 1;
-	visited[cur] = true;
-	for (auto i : v[type][cur]) {
-		if (!visited[i])
-			ret += dfs(i, type);
-	}
-	return ret;
-}
-int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
-
-	cin >> N >> M >> X;
-	while (M--) {
-		int a, b;
-		cin >> a >> b;
-		v[0][b].push_back(a);
-		v[1][a].push_back(b);
-	}
-	int ans1 = dfs(X, 0);
-	memset(visited, false, sizeof(visited));
-	int ans2 = N - dfs(X, 1) + 1;
-	cout << ans1 << " " << ans2 << "\n";
+int n,m,x,ind[100001], cnt[100001], ck[100001];
+vector <int> graph[100001];
+queue <int> q;
+map<int,int> rankMap;
+int main(){
+    cin >> n >> m >> x;
+    for(int i = 0, a,b; i < m; i++){
+        cin >> a >> b;
+        ck[a] = ck[b] = 1;
+        graph[a].push_back(b);
+        ind[b]++;
+    }
+    for(int i = 1; i <= n; i++){
+        if(!ck[i] && i == x) { cout << 1 << ' ' << n << '\n'; return 0;}
+    }
+    for(int i = 1; i <= n; i++){
+        if(!ind[i]) {
+            rankMap[i] = 1;
+            q.push(i);
+        }
+    }
+    vector <int> ans;
+    int rank = 1;
+    for(int i = 1; i < n; i++){
+        int qSize = q.size();
+        rank++;
+        while(qSize--){
+            int x = q.front();
+            q.pop();
+            ans.push_back(x);
+            for(auto next : graph[x]){
+                ind[next]--;
+                if(!ind[next]){
+                    rankMap[next] = rank;
+                    q.push(next);
+                }
+            }
+        }
+    }
+    for(auto el : rankMap) {
+        cnt[el.second]++;
+    }
+    cout << rankMap[x] << ' ' << rankMap[x] + cnt[x] - 1;
+    return 0;
 }
