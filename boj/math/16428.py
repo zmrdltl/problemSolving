fab582fda33bfdcb13d1008affe40e58e2520e40
MeenSeek Kim <meenseek5929@naver.com>
2021-09-06 22:32:31 +0900

    2021.09.06

--- boj/math/16428.py
+++ boj/math/16428.py
@@ -1,6 +1,6 @@
-a, b = map(int,input().split())
-c, d = a//b, a%b
-if a != 0 and b < 0:
+a, b = map(int, input().split())
+c, d = divmod(a, b)
+if a != 0 and d < 0:
     c, d = c+1, d-b
 print(c)
 print(d)
