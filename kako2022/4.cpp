6f76c09608dcb9c13e5dfc70ddb712f9c4adb049
MeenSeek Kim <meenseek5929@naver.com>
2021-09-11 20:33:17 +0900

    2021.09.11

---
+++ kako2022/4.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int depth, int idx){
+    
+}
+
+vector<int> solution(int n, vector<int> info) {
+    vector<int> answer;
+    return answer;
+}
