c8d086a19801528352d5e531dabb04c8eb7f37f6
MeenSeek Kim <meenseek5929@naver.com>
2021-09-11 20:33:39 +0900

    2021.09.11

---
+++ boj/Implemenation/8710.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll k, w, m; 
+int main(){
+    cin >> k >> w >> m; 
+    ll result = 0; 
+    while (k < w) k += m; result++; 
+    cout << result << "\n";
+}
