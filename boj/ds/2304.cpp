92fe453d52a7867582069c35be4b33041d0811e9
MeenSeek Kim <meenseek5929@naver.com>
2021-09-13 18:25:50 +0900

    2021.09.13

--- boj/ds/2304.cpp
+++ boj/ds/2304.cpp
@@ -8,19 +8,19 @@ int answer,maxVal;
 int n,a,b;
 int main(){
     cin>>n;
-    for(int i=0; i < n; i++){
+    for(int i = 0; i < n; i++){
         cin >> a >> b;
-        d[a][0]=b;
+        d[a][0] = b;
     }
-    for(int i = 0; i < 1000; i++){
-        maxVal = max(d[i][0],maxVal);
+    for(int i = 0; i <= 1000; i++){
+        maxVal = max(d[i][0], maxVal);
         d[i][1] = maxVal;
     }
     maxVal = 0;
     for(int i = 1000; i >= 0; i--){
-        maxVal = max(d[i][0],maxVal);
+        maxVal = max(d[i][0], maxVal);
         d[i][2] = maxVal;
     }
-    for(int i = 0; i < 1000; i++) answer += min(d[i][1],d[i][2]);
+    for(int i = 0; i <= 1000; i++) answer += min(d[i][1], d[i][2]);
     cout << answer;
 }
