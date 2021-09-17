32cb733b384ef91d1f450f327adfa910eefbd527
MeenSeek Kim <meenseek5929@naver.com>
2021-09-17 11:28:50 +0900

    2021.09.17

---
+++ boj/brute_force/13140.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int result, nums[10], flag;
+string word = "helowrd";
+unordered_map <char, int> m;
+
+int main(){
+    cin >> result;
+    for(int i = 0; i < 10; i++) nums[i] = i;
+
+    do{
+        for(int i = 0; i < word.size(); i++) m[word[i]] = nums[i];
+
+        if(m['h'] == 0 || m['w'] == 0) continue;
+
+        int a = m['h'] * 10000 + m['e'] * 1000 + m['l'] * 100 + m['l'] * 10 + m['o'];
+        int b = m['w'] * 10000 + m['o'] * 1000 + m['r'] * 100 + m['l'] * 10 + m['d'];
+
+        if(a + b == result){
+            int as = to_string(a).size();
+            int bs = to_string(b).size();
+            cout << "  " << a << '\n';
+            cout << "+ " << b << '\n';
+            cout << "-------\n";
+            for(int i = 0; i < 7 - to_string(result).size(); i++) cout << ' ';
+            cout << result << '\n';
+            flag = 1;
+            break;
+        }
+
+    } while(next_permutation(nums, nums + 10));
+    
+    if(!flag) cout << "No Answer";
+}
