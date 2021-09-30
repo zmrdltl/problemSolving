106fbde1437e8167280783f08d7c4deb77ae1ee1
MeenSeek Kim <meenseek5929@naver.com>
2021-09-30 15:42:18 +0900

    2021.09.30

---
+++ boj/ds/5766.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int n, m;
+int main(){
+    fastio;
+    while(1){
+        cin >> n >> m;
+        if(!n && !m) break;
+        vector <int> secondRankPlayer, ans;
+        map<int, int> playerScoreMap; //선수번호, 점수
+        int first = 0, second = 0;
+        for(int i = 0, x; i < n * m; i++){
+            cin >> x;
+            playerScoreMap[x]++;
+        }
+        for(auto el : playerScoreMap){
+            first = max(first, el.second);
+        }
+        for(auto el : playerScoreMap){
+            if(el.second == first) continue;
+            second = max(second, el.second);
+        }
+        for(auto el : playerScoreMap){
+            if(el.second == second) ans.push_back(el.first);
+        }
+        for(auto a : ans) cout << a << ' ';
+        cout << '\n';
+    }
+}
