8668cd9534dfdd2c4d767daf6cf6aed33dfb0c6f
zmrdltl <meenseek5929@naver.com>
2021-12-02 09:09:36 +0900

    2021.12.02

---
+++ boj/Implemenation/4562.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, t;
+int main(){
+    cin >> t;
+    while(t--){
+        cin >> a >> b;
+        if(a >= b) cout << "MMM BRAINS\n";
+        else cout << "NO BRAINS\n";
+    }
+}
