a2ae41d61546d41b84c954a78e4a47fd6269db91
MeenSeek Kim <meenseek5929@naver.com>
2021-12-05 12:09:39 +0900

    2021.12.05

--- boj/Implemenation/4635.cpp
+++ boj/Implemenation/4635.cpp
@@ -4,12 +4,10 @@ int logs;
 int main(){
     while(1){
         cin >> logs;
-        int ans = 0;
         if(logs == -1) break;
-        int speedPerHour[logs], totalDriveTime[logs];
-        for(int i = 0; i < logs; i++) {
+        int speedPerHour[logs], totalDriveTime[logs], ans = 0;
+        for(int i = 0; i < logs; i++)
             cin >> speedPerHour[i] >> totalDriveTime[i];
-        }
         for(int i = 0; i < logs; i++) {
             if(!i) ans += totalDriveTime[i] * speedPerHour[i];
             else ans += speedPerHour[i] * (totalDriveTime[i] - totalDriveTime[i-1]);
