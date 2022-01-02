e3987e1128c2e1e55de391410defab477050e839
MeenSeek Kim <meenseek5929@naver.com>
2022-01-02 19:14:35 +0900

    2022.01.02

---
+++ boj/Implemenation/6794.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int n;
+int main(){
+    cin >> n;
+    if(n == 1) cout << 1;
+    else cout << n / 2 + 1; 
+}
+
+// 1 1
+// 2 3
+
+// 3 3
+// 4 3
+
+// 5 4
+// 6 5
