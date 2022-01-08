a89bcf010dc08ceef22d21252427c2589f629cec
MeenSeek Kim <meenseek5929@naver.com>
2022-01-08 18:50:32 +0900

    2022.01.08

--- boj/Implemenation/6794.cpp
+++ boj/Implemenation/6794.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std; 
-bool visit[6]; 
+bool check[6]; 
 int n, cnt;
 int main() { 
     cin >> n; 
     for (int i = 0; i <= 5; i++)
         for (int j = 0; j <= 5; j++)
-            if (i + j == n) visit[i] = true; 
+            if (i + j == n) check[i] = true; 
     for (int i = 0; i <= n/2; i++)
-        if(visit[i]) cnt++;
+        if(check[i]) cnt++;
     cout << cnt;
 }
