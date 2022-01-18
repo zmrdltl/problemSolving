68ac62a5e1bb7198026c82a6f0e928bae65ff975
zmrdltl <meenseek5929@naver.com>
2022-01-18 11:14:45 +0900

    2022.01.18

--- boj/math/7770.cpp
+++ boj/math/7770.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, b, i;
+int allBlock, block, height;
 int main(){
-    cin >> n;
-    while(b <= n) {
-        b += 2 * i * i + 2 * i + 1;
-        i++;
+    cin >> allBlock;
+    while(block <= allBlock) {
+        block += 2 * height * height + 2 * height + 1;
+        height++;
     }
-    cout << i - 1;
+    cout << height - 1;
 }
