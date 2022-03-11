4774daa157ded257d38b7a57f926619bb202f2dd
zmrdltl <meenseek5929@naver.com>
2022-03-11 13:21:50 +0900

    2022.03.11

--- boj/Implemenation/11908.py
+++ boj/Implemenation/11908.py
@@ -1,7 +1,8 @@
 import sys
 input = sys.stdin.readline
 k, n = map(int, input().split())
-tmp = (k*n)//(n-1)
-if((k*n) % (n-1)): tmp += 1
 if(n == 1): print(-1)
-else: print(tmp)
+else:
+    ans = (k*n)//(n-1)
+    if((k*n) % (n-1)): ans += 1 
+    print(ans)
