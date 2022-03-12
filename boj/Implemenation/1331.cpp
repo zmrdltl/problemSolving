8c0316fa633d730b10c8ab79f312cb3dc92ae815
zmrdltl <meenseek5929@naver.com>
2022-03-12 19:35:32 +0900

    2022.03.12

--- boj/Implemenation/1331.cpp
+++ boj/Implemenation/1331.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int dx[8] = {1,2,2,1,-1,-2,-2,-1};
 int dy[8] = {2,1,-1,-2,-2,-1,1,2};
 bool ans = true;
-int curX, curY, oriX, oriY, board[6][6];
+int curX, curY, oriX, oriY, visited[6][6];
 string s;
 vector <string> v;
 
@@ -21,7 +21,7 @@ bool canGo(int curx, int cury, int x, int y){
 bool isAllVisited(){
     for(int i = 0; i < 6; i++){
         for(int j = 0; j < 6; j++){
-            if(!board[i][j]) return false;
+            if(!visited[i][j]) return false;
         }
     }
     return true;
@@ -34,19 +34,16 @@ int main(){
     curY = s[1] - '1';
     oriX = curX;
     oriY = curY;
-    board[oriX][oriY] = 1;
+    visited[oriX][oriY] = 1;
     for(int i = 1; i < 36; i++){
         cin >> s;
-        v[i] = s;
-    }
-    for(int i = 1; i < 36; i++){
         int x, y;
-        x = v[i][0] - 'A';
-        y = v[i][1] - '1';
-        if(!canGo(curX, curY, x, y)) {ans = false; break;}
+        x = s[0] - 'A';
+        y = s[1] - '1';
+        if(!canGo(curX, curY, x, y)) ans = false;
         curX = x;
         curY = y;
-        board[curX][curY] = 1;
+        visited[curX][curY] = 1;
     }
     if(ans) {
         if(canGo(curX, curY, oriX, oriY) && isAllVisited()) cout << "Valid";
