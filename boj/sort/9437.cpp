798a63b2148370984f96de560f063a24a0221ba1
zmrdltl <meenseek5929@naver.com>
2022-03-14 10:27:01 +0900

    2022.03.14

--- boj/sort/9437.cpp
+++ boj/sort/9437.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int n, cnt[101], allGived = 3;
-vector<vector<int>> v;
+vector<vector<int>> info;
 
 bool cmp(vector<int> &a, vector<int> &b){
     return a[2] > b[2];
@@ -13,11 +13,11 @@ int main(){
     for(int i = 1; i <= n; i++){
         int countryNum, studentNum, score;
         cin >> countryNum >> studentNum >> score;
-        v.push_back({countryNum, studentNum, score});
+        info.push_back({countryNum, studentNum, score});
         cnt[i] = 2;
     }
-    sort(v.begin(), v.end(), cmp);
-    for(auto e : v) {
+    sort(info.begin(), info.end(), cmp);
+    for(auto e : info) {
         if(!allGived) break;
         if(cnt[e[0]] > 0){
             cout << e[0] << ' ' << e[1] << '\n';
