0dbcbac6c607d6b79581471ade839ace7922e9d2
zmrdltl <meenseek5929@naver.com>
2022-04-03 14:37:08 +0900

    2022.04.03

---
+++ boj/graph/bfs/1937.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using tiii = tuple<int,int,int>;
+
+int dr[] = {0,0,1,-1}, dc[] = {1,-1,0,0};
+int bamboo[501][501], live[501][501], n, ans;
+
+int dp(int r, int c){
+    int &ret = live[r][c];
+    if(ret) return ret;
+    ret = 1;
+    for(int i = 0; i < 4; i++){
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if(0 > nr || nr >= n || 0 > nc || nc >= n) continue;
+        if(bamboo[r][c] >= bamboo[nr][nc]) continue;
+        ret = max(ret,dp(nr, nc) + 1);
+    }
+    return ret;
+}
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> bamboo[i][j];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            ans = max(ans,dp(i,j));
+        }
+    }
+    cout << ans;
+}
