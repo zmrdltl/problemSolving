771cf5b925bed142c078b647c6427f138d43a6d7
zmrdltl <meenseek5929@naver.com>
2022-04-06 09:42:33 +0900

    2022.04.06

--- boj/greedy/13416.cpp
+++ boj/greedy/13416.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int t, n, ans;
+int t, n, ans, a, b, c;
 int main(){
     cin >> t;
     while(t--){
         cin >> n;
         ans = 0;
-        for(int i = 0, a, b, c; i < n; i++){
+        for(int i = 0; i < n; i++){
             cin >> a >> b >> c;
             int profit = max({a,b,c});
             if(profit <= 0) continue;
