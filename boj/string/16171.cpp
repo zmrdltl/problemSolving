2f9c997734348bae998b1b1be72f35ec1a46dea6
zmrdltl <meenseek5929@naver.com>
2022-04-15 09:26:32 +0900

    2022.04.15

--- boj/string/16171.cpp
+++ boj/string/16171.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 string s, k;
-map <char, int> m;
+map <char, int> filteringMap;
 
 string getFilteredString(){
     string filteredString;
     for(int i = 0; i < s.size(); i++)
-        if(!m[s[i]]) 
+        if(!filteringMap[s[i]]) 
             filteredString += s[i];
     return filteredString;
 }
 
 int main(){
     cin >> s >> k;
-    for(int i = 0; i <= 9; i++) m[i + '0'] = 1;
+    for(int i = 0; i <= 9; i++) filteringMap[i + '0'] = 1;
     s = getFilteredString();
     if(s.find(k) != string::npos) cout << 1;
     else cout << 0;
