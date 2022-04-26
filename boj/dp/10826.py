b1d298f67eecb028df012fe33058ed5579861a87
zmrdltl <meenseek5929@naver.com>
2022-04-26 09:51:34 +0900

    2022.04.26

--- boj/dp/10826.py
+++ boj/dp/10826.py
@@ -2,9 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-fibo = [0,1,1]
+fibo = [0,1]
 
-for i in range(3, n+1):
+for i in range(2, n+1):
     fibo.append(fibo[i-1] + fibo[i-2])
     
 print(fibo[n])
