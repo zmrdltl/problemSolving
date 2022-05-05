91a0eed7bb6730f5e006421287dcdf1ed3f46fe5
zmrdltl <meenseek5929@naver.com>
2022-05-06 08:48:17 +0900

    2022.05.06

--- boj/dp/19947.cpp
+++ boj/dp/19947.cpp
@@ -7,9 +7,9 @@ int backtracking(double h, int y){
   if(y < 0) return 0;
   if(y == 0) return h;
   int ret = 0;
-  ret = max(ret, dp((int)h * 1.05, y - 1));
-  ret = max(ret, dp((int)h * 1.2, y - 3));
-  ret = max(ret, dp((int)h * 1.35, y - 5));
+  ret = max(ret, backtracking((int)h * 1.05, y - 1));
+  ret = max(ret, backtracking((int)h * 1.2, y - 3));
+  ret = max(ret, backtracking((int)h * 1.35, y - 5));
 
   return ret;
 }
