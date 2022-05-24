40b91aa11e8d63c25c392f36b5e7c24122b8d5ca
zmrdltl <meenseek5929@naver.com>
2022-05-24 09:45:47 +0900

    2022.05.24

--- boj/Implemenation/10384.cpp
+++ boj/Implemenation/10384.cpp
@@ -1,24 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string getResultStr(string s){
-  int pengramCnt = 10;
-  vector <int> alpha(26,0);
-  string str = getFilteredStr(s);
-  for(auto c : str){
-    alpha[c-'a']++;
-  }
-
-  for(int i = 0; i < 26; i++){
-    pengramCnt = min(pengramCnt, alpha[i]);
-  }
-
-  if(!pengramCnt) return "Not a pangram\n";
-  if(pengramCnt == 1) return "Pangram!\n";
-  if(pengramCnt == 2) return "Double pangram!!\n";
-  return "Triple pangram!!!\n";
-}
-
 string getFilteredStr(string s){
   string str;
   for(auto c : s){
@@ -26,6 +8,24 @@ string getFilteredStr(string s){
     else if('a' <= c && c <= 'z') str += c;
   }
   return str;
+}
+
+string getResultStr(string s){
+  int pangramCnt = 10;
+  vector <int> alpha(26,0);
+  string str = getFilteredStr(s);
+  for(auto c : str){
+    alpha[c-'a']++;
+  }
+
+  for(int i = 0; i < 26; i++){
+    pangramCnt = min(pangramCnt, alpha[i]);
+  }
+
+  if(!pangramCnt) return "Not a pangram\n";
+  if(pangramCnt == 1) return "Pangram!\n";
+  if(pangramCnt == 2) return "Double pangram!!\n";
+  return "Triple pangram!!!\n";
 }
 
 int main(){
