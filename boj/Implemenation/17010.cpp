12c8110eb194c478a73fce0462890e8d113116b7
zmrdltl <meenseek5929@naver.com>
2022-06-11 17:48:49 +0900

    2022.06.11

---
+++ boj/Implemenation/17010.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+char c;
+
+int main(){
+  cin >> t;
+  while(t--){
+    cin >> n >> c;
+    while(n--) cout << c;
+    cout << '\n';
+  }
+}
