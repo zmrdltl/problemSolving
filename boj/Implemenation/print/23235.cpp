11dbd61c923fffda72f1804b2b9cad1359efefb2
zmrdltl <meenseek5929@naver.com>
2022-06-17 09:59:55 +0900

    2022.06.17

---
+++ boj/Implemenation/print/23235.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main(){
+  for(int i = 1;;i++){
+    getline(cin, s);
+    if(s == "0") break;
+    printf("Case %d: Sorting... done!\n", i);
+  }
+}
