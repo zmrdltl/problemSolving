ba9ac109978f80f4cceac0f27c4e7e83ba417856
zmrdltl <meenseek5929@naver.com>
2022-06-20 10:23:17 +0900

    2022.06.20

--- boj/brute_force/14625.cpp
+++ boj/brute_force/14625.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int startHour, startMinute, endHour, endMinute, s, e, num, ans;
 
-string getMinString(int minute){
-  string minString;
-  string hourStr = to_string(minute/60);
+string getTimeString(int minute){
+  string minString, minuteStr, hourStr;
+
+  hourStr = to_string(minute/60);
   if(hourStr.size() == 1) minString += "0" + hourStr;
   else minString += hourStr;
 
-  string minuteStr = to_string(minute%60);
+  minuteStr = to_string(minute%60);
   if(minuteStr.size() == 1) minString += "0" + minuteStr;
   else minString += minuteStr;
 
@@ -26,9 +27,9 @@ int main(){
   cin >> startHour >> startMinute >> endHour >> endMinute >> num;
   s = startHour * 60 + startMinute;
   e = endHour * 60 + endMinute;
-  for(int i = s; i <= e; i++){
-    string curMinute = getMinString(i);
-    if(isNumExist(curMinute)) ans++;
+  for(int minute = s; minute <= e; minute++){
+    string curTime = getTimeString(minute);
+    if(isNumExist(curTime)) ans++;
   }
   cout << ans;
 }
