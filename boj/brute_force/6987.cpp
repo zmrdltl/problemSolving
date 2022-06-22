080b0b832f58464af3dcebf5ecccec85e61d5501
MeenSeek Kim <meenseek5929@naver.com>
2022-06-22 21:11:01 +0900

    2022.06.22

--- boj/brute_force/6987.cpp
+++ boj/brute_force/6987.cpp
@@ -1,66 +1,59 @@
 #include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-using pii = pair<int,int>;
-vector <vector<int>> info(6);
 
-bool cmp(pii a, pii b){
-  return a.second > b.second;
-}
+struct VS{
+  int home, away;
+};
 
-int can(){
-  vector <int> loseCnt(6,0), drawCnt(6,0);
-  vector <pair<int,int>> v, v2;
-  for(int i = 0; i < 6; i++){
-    v.push_back({i,info[i][2]});
-  } 
-  sort(v.begin(), v.end(), cmp);
+int result[10][10], madeResult[10][10];
+vector <VS> vs;
 
-  for(int i = 0; i < 6; i++){
-    v2.push_back({i,info[i][1]});
-  } 
-  sort(v2.begin(), v2.end(), cmp);
-  
-
-  for(int i = 0; i < 6; i++){
-    int win = info[i][0];
-    for(int j = 0; j < 6; j++){
-      if(i == v[j].first || info[v[j].first][2] == loseCnt[v[j].first]) continue;
-      if(!win) break;
-      loseCnt[v[j].first]++;
-      win--;
+int dfs(int played){
+  if(played == 15){
+    for(int i = 0; i < 6; i++){
+      for(int j = 0; j < 3; j++){
+        if(result[i][j] != madeResult[i][j]) return 0;
+      }
     }
+    return 1;
   }
 
-  for(int i = 0; i < 6; i++){
-    int draw = info[i][1];
-    for(int j = 0; j < 6; j++){
-      if(i == v2[j].first || info[v2[j].first][1] == drawCnt[v2[j].first]) continue;
-      if(!draw) break;
-      drawCnt[v2[j].first]++;
-      draw--;
-    }
-  }
+  int ans = 0;
+  int home = vs[played].home;
+  int away = vs[played].away;
+  //승패
+  madeResult[home][0]++; madeResult[away][2]++;
+  ans = max(ans,dfs(played + 1));
+  madeResult[home][0]--; madeResult[away][2]--;
 
-  for(int i = 0; i < 6; i++){
-    if(loseCnt[i] != info[i][2] || info[i][1] != drawCnt[i]) return 0;
-  }
+  //패승
+  madeResult[away][0]++; madeResult[home][2]++;
+  ans = max(ans,dfs(played + 1));
+  madeResult[away][0]--; madeResult[home][2]--;
 
-  return 1;
+  //무무
+  madeResult[away][1]++; madeResult[home][1]++;
+  ans = max(ans,dfs(played + 1));
+  madeResult[away][1]--; madeResult[home][1]--;
+
+  return ans;
 }
 
 int main(){
-  for(int i = 0; i < 4; i++){
-
-    info.clear();
-
-    for(int k = 0; k < 6; k++){
-      vector <int> countryInfo(3);
-      for(int j = 0; j < 3; j++){
-        cin >> countryInfo[j];
-      }
-      info.push_back(countryInfo);
+  fastio;
+  for(int i = 0; i < 6; i++){
+    for(int j = i + 1; j < 6; j++){
+      vs.push_back({i,j});
     }
-
-    cout << can() << '\n';
+  }
+  for(int t = 0; t < 4; t++){
+    memset(madeResult,0,sizeof(madeResult));
+    for(int i = 0; i < 6; i++){
+      for(int j = 0; j < 3; j++){
+        cin >> result[i][j];
+      }
+    }
+    cout << dfs(0) << ' ';
   }
 }
