f7be095957439240f2aa4318b17e29f7683425ac
MeenSeek Kim <meenseek5929@naver.com>
2022-06-30 18:32:32 +0900

    2022.06.30

--- programmers/위클리챌린지/5주차.cpp
+++ programmers/위클리챌린지/5주차.cpp
@@ -1,34 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector <string> dictionary;
-string allChar = "AEIOU";
-map <string,int> m;
-bool cmp(string a, string b){
-    for(int i = 0; i < a.size(); i++)
-        if(a[i] < b[i] && i < b.size()) 
-            return a[i] < b[i];
-    return (int)a.size() < (int)b.size();
-}
+string str = "AEIOU";
+map <string,int> comb;
 
-void dfs(int depth, string word, int limit){
-    if(depth == limit){
-        m[word] = 1;
+void dfs(int depth, int want, string tmp){
+    if(depth == want){
+        comb[tmp] = 1;
         return;
     }
-    for(int i = 0; i < 5; i++) dfs(depth + 1, word + allChar[i], limit);
+    for(int i = 0; i < 5; i++){
+        dfs(depth + 1, want, tmp + str[i]);
+    }
 }
 
 int solution(string word) {
-    int answer = 1;
-    dictionary.clear();
-    m.clear();
-    for(int i = 1; i <= 5; i++)
-        dfs(0,"",i);
-
-    for(auto el : m) dictionary.push_back(el.first);
-
-    for(auto d : dictionary) {
-        if(d == word) return answer;
+    comb.clear();
+    int answer = 0;
+    
+    for(int i = 1; i <= 5; i++) dfs(0,i,"");
+    
+    for(auto c : comb){
         answer++;
+        if(c.first == word) return answer;
     }
+    
+    return answer;
 }
