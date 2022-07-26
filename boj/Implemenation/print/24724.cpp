e31f9157dd185b9dc9c4b75c08df4b3f6665c969
zmrdltl <meenseek5929@naver.com>
2022-07-26 10:19:15 +0900

    2022.07.26

---
+++ boj/Implemenation/print/24724.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t, n, l1, l2, a, b;
+int main(){
+  cin >> t;
+  for(int i = 1; i <= t; i++){
+    cin >> n >> l1 >> l2;
+    for(int j = 0; j < n; j++) cin >> a >> b;
+    cout << "Material Management " << i << '\n';
+    cout << "Classification ---- End!\n";
+  }
+}
