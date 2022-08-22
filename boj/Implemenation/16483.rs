ad7ae1fc48c0b32cd931f47e3e7f335a75b92bc4
zmrdltl <meenseek5929@naver.com>
2022-08-22 09:51:34 +0900

    2022.08.22

---
+++ boj/Implemenation/16483.rs
@@ -0,0 +1,8 @@
+use std::io;
+fn main() {
+  let mut line = String::new();
+  io::stdin().read_line(&mut line).expect("wrong io");
+  let t = line.trim().parse::<f64>().unwrap();
+  println!("{}", (t/2.0 * t/2.0).round());
+
+}
