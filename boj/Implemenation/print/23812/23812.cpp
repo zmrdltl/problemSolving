84774b6fa2c468ad640e48a4026671f79e706d9e
zmrdltl <meenseek5929@naver.com>
2022-09-22 13:43:02 +0900

    2022.09.22

---
+++ boj/Implemenation/print/23812/23812.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+
+void printOther(){
+  for(int i = 0; i < n; i++){
+    for(int j = 1; j <= n * 5; j++){
+      if(j <= n || j > n * 4) cout << '@';
+      else cout << ' ';
+    }
+    cout << '\n';
+  }
+}
+
+void printSquare(){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n * 5; j++){
+      cout << '@';
+    }
+    cout << '\n';
+  }
+}
+
+int main(){
+  cin >> n;
+  printOther();
+  printSquare();
+  printOther();
+  printSquare();
+  printOther();
+}
