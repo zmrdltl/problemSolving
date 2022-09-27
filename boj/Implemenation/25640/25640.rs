cc8c5f6fb22c4e88e8daabca3ab030606e6fce48
zmrdltl <meenseek5929@naver.com>
2022-09-27 09:42:28 +0900

    2022.09.27

--- boj/Implemenation/25640/25640.rs
+++ boj/Implemenation/25640/25640.rs
@@ -3,15 +3,15 @@ fn main(){
   let mut line = String::new();
   let mut ans = 0;
   io::stdin().read_line(&mut line).expect("wrong io");
-  let jinhoMBTI = line.trim();
+  let jinho_mbti = line.trim();
   let mut line = String::new();
   io::stdin().read_line(&mut line).expect("wrong io");
   let n = line.trim().parse::<i64>().unwrap();
   for _ in 0..n {
     let mut line = String::new();
     io::stdin().read_line(&mut line).expect("wrong io");
-    let friendMBTI = line.trim();
-    if jinhoMBTI == friendMBTI {
+    let friend_mbti = line.trim();
+    if jinho_mbti == friend_mbti {
       ans+=1;
     }
   }
