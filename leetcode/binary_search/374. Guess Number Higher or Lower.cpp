4929accc708ab4654ab3e318c8a586c13d082418
zmrdltl <meenseek5929@naver.com>
2022-11-16 10:00:21 +0900

    2022.11.16

---
+++ leetcode/binary_search/374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        ll l = 1;
+        ll r = n;
+        while(l<=r){
+            ll mid = (l+r)/2;
+            int res = guess(mid);
+            if(res == -1) r = mid - 1;
+            else if(res == 1) l = mid + 1;
+            else return mid;
+        }
+        return n;
+    }
+};
