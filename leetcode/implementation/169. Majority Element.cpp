356cb220c8251c71669cc116a8a65b794e0706d5
zmrdltl <meenseek5929@naver.com>
2022-12-09 10:40:26 +0900

    2022.12.09

--- leetcode/implementation/169. Majority Element.cpp
+++ leetcode/implementation/169. Majority Element.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int ans = 0;
         int majorLimit = ceil((double)nums.size() / 2.0);
         map<int,int> frequencyMap;
-        int ans = 0;
         for(auto n : nums) {
             frequencyMap[n]++;
         }
