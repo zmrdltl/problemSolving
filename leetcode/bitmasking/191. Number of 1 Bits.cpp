a75cd7013eb5405167314356c726ee352d75eb9b
zmrdltl <meenseek5929@naver.com>
2022-12-17 20:03:26 +0900

    2022.12.17

---
+++ leetcode/bitmasking/191. Number of 1 Bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        for(int i = 0; i < 32; i++) {
+            if(n & (1 << i)) cnt++;
+        }
+        return cnt;
+    }
+};
