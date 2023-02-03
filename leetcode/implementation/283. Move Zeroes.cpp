5711e8903cdd9942f58610d2438ba44ef0658cca
zmrdltl <meenseek5929@naver.com>
2023-02-03 13:11:24 +0900

    2023.02.03

--- leetcode/implementation/283. Move Zeroes.cpp
+++ leetcode/implementation/283. Move Zeroes.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeroCnt = 0;
         int nonZeroIdx = 0;
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] != 0) {
