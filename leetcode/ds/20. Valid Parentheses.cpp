7aebed557df2a5a9a78fd70a183c6c9dc36e0a98
zmrdltl <meenseek5929@naver.com>
2023-04-10 09:15:05 +0900

    2023.04.10

--- leetcode/ds/20. Valid Parentheses.cpp
+++ leetcode/ds/20. Valid Parentheses.cpp
@@ -3,22 +3,21 @@ public:
     bool isValid(string s) {
         stack <char> st;
         for(auto c : s) {
-            if(c == '(' || c == '[' || c == '{') {
-                st.push(c);
-                continue;
+            if(st.size()) {
+                if(st.top() == '(' && c == ')') {
+                    st.pop();
+                    continue;
+                }
+                if(st.top() == '{' && c == '}') {
+                    st.pop();
+                    continue;
+                }
+                if(st.top() == '[' && c == ']') {
+                    st.pop();
+                    continue;
+                }
             }
-            if(st.size() && 
-               ((st.top() == '(' && c == ')') || 
-                (st.top() == '[' && c == ']') || 
-                (st.top() == '{' && c == '}')
-               )){
-                st.pop();
-                continue;
-            }
-            else {
-                st.push(c);
-                break;
-            }
+            st.push(c);
         }
         return st.size() == 0;
     }
