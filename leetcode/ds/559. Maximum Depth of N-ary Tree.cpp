4370bf7a430489bce2a565bd52cb9cbb555a589e
zmrdltl <meenseek5929@naver.com>
2023-04-20 10:00:39 +0900

    2023.04.20

---
+++ leetcode/ds/559. Maximum Depth of N-ary Tree.cpp
@@ -0,0 +1,31 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root == NULL) return 0;
+        int length = 0;
+        for(auto nextNode : root->children) {
+            length = max(length, maxDepth(nextNode));
+        }
+        return length + 1;
+    }
+};
