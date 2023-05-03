c93781f634fd7ca9ab71c28e338fd51855838b0d
zmrdltl <meenseek5929@naver.com>
2023-05-03 09:49:27 +0900

    2023.05.03

--- leetcode/sql/586. Customer Placing the Largest Number of Orders.sql
+++ leetcode/sql/586. Customer Placing the Largest Number of Orders.sql
@@ -9,3 +9,13 @@ select customer_number from orders
 group by customer_number
 order by count(*) desc
 limit 1
+
+-------- oracle
+SELECT customer_number
+FROM (
+    SELECT customer_number
+    FROM orders
+    GROUP BY customer_number
+    ORDER BY COUNT(*) DESC
+)
+WHERE ROWNUM = 1;
