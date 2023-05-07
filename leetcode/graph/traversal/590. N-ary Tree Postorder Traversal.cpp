0d0bf4fdb861206e2d2718e5574f24d959f5505a
zmrdltl <meenseek5929@naver.com>
2023-05-07 14:00:30 +0900

    2023.05.07

---
+++ leetcode/graph/traversal/590. N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if(root == NULL) return vector<int>();
+        vector <int> ans;
+        for(auto c : root->children) {
+            vector<int>child= postorder(c);
+            ans.insert(ans.end(), child.begin(), child.end());
+        }
+
+        ans.push_back(root->val);
+
+        return ans;
+    }
+};
