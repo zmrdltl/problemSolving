2f7a06d0c267d88b1eedabf6af8a1819f40027cd
zmrdltl <meenseek5929@naver.com>
2023-06-05 10:02:19 +0900

    2023.06.05

---
+++ leetcode/implementation/661. Image Smoother.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int getAvg(int r, int c, int rLimit, int cLimit, vector<vector<int>>& img) {
+        int cnt = 0;
+        int sum = 0;
+        for(int k = r-1; k <= r + 1; k++) {
+            if(k < 0 || k >= rLimit) continue;
+            for(int l = c - 1; l <= c + 1; l++) {
+                if(l < 0 || l >= cLimit) continue;
+                cnt++;
+                sum += img[k][l];
+            }
+        }
+        return sum / cnt;
+    }
+    
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int r = img.size();
+        int c = img[0].size();
+        vector<vector<int>> v;
+        for (int i = 0; i < r; i++) {
+            vector <int> rows;
+
+            for (int j = 0; j < c; j++) {
+                
+
+                int avg = getAvg(i, j, r, c, img);
+                rows.push_back(avg);
+            }
+            v.push_back(rows);
+        }
+        return v;
+
+    }
+};
