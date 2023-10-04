425de93eeec693ee6ac6b7641b359f0ca6afe120
zmrdltl <meenseek5929@naver.com>
2023-10-04 17:50:17 +0900

    2023.10.04

--- leetcode/implementation/1013. Partition Array Into Three Parts With Equal Sum/1013. Partition Array Into Three Parts With Equal Sum.rs
+++ leetcode/implementation/1013. Partition Array Into Three Parts With Equal Sum/1013. Partition Array Into Three Parts With Equal Sum.rs
@@ -1,14 +1,17 @@
+use std::convert::TryInto;
 impl Solution {
     pub fn can_three_parts_equal_sum(arr: Vec<i32>) -> bool {
         let mut sum: i32 = arr.iter().sum();
-        if sum % 3 != 0 return false;
+        if sum % 3 != 0 {
+            return false;
+        }
         let piv_sum = sum / 3;
         sum = 0;
-        let pivIdx:Vec<i32> = Vec::new();
+        let mut piv_idx:Vec<i32> = Vec::new();
         for (i, &num) in arr.iter().enumerate() {
             sum += num;
             if sum == piv_sum {
-                piv_idx.push(i);
+                piv_idx.push(i.try_into().unwrap());
                 sum = 0;
             }
         }
