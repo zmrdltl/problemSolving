d3880b26644791a801132a27394ca7e3cb3a34aa
zmrdltl <meenseek5929@naver.com>
2023-10-26 09:43:34 +0900

    2023.10.26

---
+++ leetcode/sql/1084. Sales Analysis III.sql
@@ -0,0 +1,5 @@
+select p.product_id, p.product_name from sales s
+join product p
+on p.product_id = s.product_id
+group by p.product_id, p.product_name
+having sum(case when s.sale_date not between '2019-01-01' and '2019-03-31' then 1 else 0 end) = 0
