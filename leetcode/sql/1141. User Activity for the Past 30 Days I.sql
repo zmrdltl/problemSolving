23d24a4a54e18f24c240057bc19de87e8552a50c
zmrdltl <meenseek5929@naver.com>
2023-11-07 09:35:58 +0900

    2023.11.07

---
+++ leetcode/sql/1141. User Activity for the Past 30 Days I.sql
@@ -0,0 +1,9 @@
+SELECT 
+    activity_date AS day, 
+    COUNT(DISTINCT user_id) AS active_users 
+FROM 
+    activity
+WHERE 
+    activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY 
+    1;
