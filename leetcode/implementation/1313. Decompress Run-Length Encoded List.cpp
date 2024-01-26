fd7e6ad077ab04ebee7113288f96e2c2b3c15a9b
meenseek_kim <meenseek_kim@tmax.co.kr>
2024-01-26 10:14:57 +0900

    2024.01.26

---
+++ leetcode/implementation/1313. Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        
+    }
+};class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector <int> ans;
+        for(int i = 0; i < nums.size() - 1; i+=2) {
+            int freq = nums[i];
+            int val = nums[i+1];
+            for(int j = 0; j < freq; j++) ans.push_back(val);
+        }
+        return ans;
+    }
+};
