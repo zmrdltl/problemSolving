f22694da9c5883dfc7ed11ff27f37ce9676281f9
meenseek_kim <meenseek_kim@tmax.co.kr>
2024-04-17 14:55:20 +0900

    2024.04.17

--- leetcode/math/1523. Count Odd Numbers in an Interval Range.cpp
+++ leetcode/math/1523. Count Odd Numbers in an Interval Range.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int ans = 0;
+        int ans = (high - low) / 2;
         if (low % 2) {
-            ans = (high - low) / 2 + 1; 
+            ans = 1; 
         } else {
-            ans = (high - low) / 2;
             if(high % 2) ans++;
         }
         return ans;
