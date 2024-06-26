72b70a58c3e8cc66dc08e4287447c4b1b29d083d
meenseek_kim <meenseek_kim@tmax.co.kr>
2024-06-26 09:20:51 +0900

    2024.06.26

--- leetcode/sql/1731. The Number of Employees Which Report to Each Employee.sql
+++ leetcode/sql/1731. The Number of Employees Which Report to Each Employee.sql
@@ -1,5 +1,5 @@
 select e1.employee_id, e1.name, count(e1.employee_id) as reports_count, round(avg(e2.age)) as average_age
 from employees e1 
 join employees e2 on e1.employee_id = e2.reports_to
-group by e1.employee_id, name
-order by employee_id
+group by e1.employee_id, e1.name
+order by e1.employee_id
