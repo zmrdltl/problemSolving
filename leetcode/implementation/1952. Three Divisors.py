c9e7d0715978d59410c3be6f86488e80c0fdcacd
meenseek_kim <meenseek_kim@tmax.co.kr>
2024-09-30 09:08:59 +0900

    2024.09.30

---
+++ leetcode/implementation/1952. Three Divisors.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        divisors = 0
+        for i in range(1, n+1):
+            if n % i == 0: divisors += 1
+        return divisors == 3
