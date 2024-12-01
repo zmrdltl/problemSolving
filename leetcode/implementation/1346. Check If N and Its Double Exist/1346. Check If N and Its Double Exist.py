b9cc791a9dc0efdcc6f18374b2918b9d52e802b3
zmrdltl <meenseek5929@naver.com>
2024-12-01 20:52:19 +0900

    2024.12.01

---
+++ leetcode/implementation/1346. Check If N and Its Double Exist/1346. Check If N and Its Double Exist.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(0, len(arr)):
+            for j in range(0, len(arr)):
+                if i == j:
+                    continue
+                if arr[i] == 2 * arr[j]:
+                    return True
+        return False
