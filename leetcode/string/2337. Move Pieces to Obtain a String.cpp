06d92bf246ccae780e42ca19f2cde200c1b8ef44
zmrdltl <meenseek5929@naver.com>
2024-12-05 22:20:51 +0900

    2024.12.05

---
+++ leetcode/string/2337. Move Pieces to Obtain a String.cpp
@@ -0,0 +1,21 @@
+from bisect import bisect_left
+
+class Solution:
+  def canChange(self, start: str, target: str) -> bool:
+    length = len(start)
+    start_char_idx = [(i,char) for i,char in enumerate(start) if char != '_']
+    target_char_idx = [(i,char) for i,char in enumerate(target) if char != '_']
+
+    print(start_char_idx)
+    print(target_char_idx)
+
+    if len(start_char_idx) != len(target_char_idx):
+        return False
+    if any(s[1] != t[1] for s, t in zip(start_char_idx, target_char_idx)):
+        return False
+    for (start_idx, start_char), (target_idx, _) in zip(start_char_idx, target_char_idx):
+        if start_char == 'L' and start_idx < target_idx:
+            return False
+        elif start_char == 'R' and start_idx > target_idx:
+            return False
+    return True
