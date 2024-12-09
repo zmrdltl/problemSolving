ea83de82813c10b0bb2038eebaa689268c1f02a2
zmrdltl <meenseek5929@naver.com>
2024-12-09 18:35:00 +0900

    2024.12.09

--- boj/graph/dijkstra/1939/1939.py
+++ boj/graph/dijkstra/1939/1939.py
@@ -51,24 +51,19 @@ start, end = map(int, input().split())
 # 다익스트라 알고리즘
 def dijkstra():
     max_heap = []
-    heapq.heappush(max_heap, (-10**9, start))  # 최대 힙: (현재까지 가능한 최대 중량, 현재 노드)
-    max_weight = [0] * (n + 1)
-    max_weight[start] = 10**9  # 시작 노드의 중량 제한은 무한대
-
+    heapq.heappush(max_heap, (-10**9, start))
+    max_weight = [0] * (n+1)
+    max_weight[start] = 10**9
     while max_heap:
         weight, node = heapq.heappop(max_heap)
-        weight = -weight  # 최대 힙 사용을 위해 음수로 저장한 값을 되돌림
-
+        weight = -weight
         if node == end:
-            return weight  # 도착 지점에 도달하면 최대 중량 반환
-
+            return weight
         for next_node, limit in graph[node]:
-            # 현재까지의 최소 중량 제한과 다음 다리의 중량 제한 중 작은 값을 사용
             next_weight = min(weight, limit)
-            if next_weight > max_weight[next_node]:  # 더 큰 중량 제한으로 갱신 가능한 경우
+            if max_weight[next_node] < next_weight:
                 max_weight[next_node] = next_weight
                 heapq.heappush(max_heap, (-next_weight, next_node))
-
-    return 0  # 연결되지 않은 경우 (문제 조건상 항상 연결되므로 실행되지 않음)
+    return 0
 
 print(dijkstra())
