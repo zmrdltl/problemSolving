41412cc440a8774913cf126d182266a1ef9ffe40
zmrdltl <meenseek5929@naver.com>
2024-12-22 15:35:33 +0900

    2024.12.22

---
+++ leetcode/implementation/1974. Minimum Time to Type Word Using Special Typewriter.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minTimeToType(self, word: str) -> int:
+        piv = 'a'
+        move = 0
+        for char in list(word):
+            clockwise_move = ord(char) - ord(piv)
+            if clockwise_move < 0:
+                clockwise_move += 26
+            counter_clockwise_move = ord(piv) - ord(char)
+            if counter_clockwise_move < 0:
+                counter_clockwise_move += 26
+            move += min(clockwise_move,counter_clockwise_move)
+            piv = charx
+            move+=1
+        return move
