e7c8b39fec1d53f6df46250a5d922775a33f6e1d
zmrdltl <meenseek5929@naver.com>
2024-12-29 17:03:43 +0900

    2024.12.29

---
+++ boj/dp/1639. Number of Ways to Form a Target String Given a Dictionary.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        MOD = 10**9 + 7
+        word_len = len(words[0])
+        target_len = len(target)
+        freq_map = [defaultdict(int) for _ in range(word_len)]
+        for word in words:
+            for j, char in enumerate(word):
+                freq_map[j][char] += 1
+        memo = defaultdict(int)
+        def dp(j, k):
+            if j == target_len:
+                return 1
+            if k == word_len:
+                return 0
+            if (j,k) in memo:
+                return memo[j,k]
+            ways = dp(j,k+1)
+            ways += freq_map[k][target[j]] * dp(j+1,k+1) % MOD
+            memo[(j,k)] = ways % MOD
+            return memo[(j,k)]
+        return dp(0, 0)
