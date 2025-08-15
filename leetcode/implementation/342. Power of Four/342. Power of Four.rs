9946a97b096ae9021197bf2720b2421b111131d9
john <john@cluml.com>
2025-08-15 22:41:14 +0900

    2025.08.15

--- leetcode/implementation/342. Power of Four/342. Power of Four.rs
+++ leetcode/implementation/342. Power of Four/342. Power of Four.rs
@@ -3,7 +3,7 @@ impl Solution {
         if n == 0 {
             return false;
         }
-        for i in 0..=21 {
+        for i in 0..=16 {
             if 4_i32.pow(i) == n {
                 return true;
             }
