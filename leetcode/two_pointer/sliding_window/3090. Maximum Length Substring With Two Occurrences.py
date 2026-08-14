9b85b21b5d25127ea402a4ab7bdef12a2be2a4df
zmrdltl <meenseek5929@naver.com>
2026-08-14 20:40:25 +0900

    2026.08.14

--- leetcode/two_pointer/sliding_window/3090. Maximum Length Substring With Two Occurrences.py
+++ leetcode/two_pointer/sliding_window/3090. Maximum Length Substring With Two Occurrences.py
@@ -1,9 +1,7 @@
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
-        l = 0
-        r = 0
+        l,r,ans = 0,0,0
         freq = {}
-        ans = 0
         while r < len(s):
             freq[s[r]] = freq.get(s[r],0) + 1
             while freq[s[r]] > 2:
